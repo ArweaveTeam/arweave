26cb4b5d7ba1a7248dfe805b5f1317851ed5f766
Sam Williams <sam@archain.org>
2017-10-17 16:29:06 +0100

    - Fixed some data definitions in ar_seralize.

--- src/ar_serialize.erl
+++ src/ar_serialize.erl
@@ -9,84 +9,90 @@
 %% @doc Translate a block into json for HTTP
 block_to_json(
 	#block {
+		nonce = Nonce,
+		timestamp = TimeStamp,
+		last_retarget = LastRetarget,
+		diff = Diff,
 		height = Height,
 		hash = Hash,
-		nonce = Nonce,
+		indep_hash = IndepHash,
 		txs = Txs,
-		wallet_list = Wallet_list,
-		hash_list = Hash_list
+		hash_list = Hash_list,
+		wallet_list = Wallet_list
 	}) ->
-		EncodedB =
-		 {struct,
-		[
-			{height, Height},
-			{hash, Hash},
-			{nonce, base64:encode(Nonce)},
-			{txs, {array, lists:map(fun tx_to_json/1, Txs) }},
-			{wallet_list, Wallet_list},
-			{hash_list, Hash_list}
-		]
-	},
-		json2:encode(EncodedB).
+	EncodedB =
+		{struct,
+			[
+				{nonce, base64:encode_to_string_to_string(Nonce)},
+				{timestamp, TimeStamp},
+				{last_retarget, LastRetarget},
+				{diff, Diff},
+				{height, Height},
+				{hash, Hash},
+				{indep_hash, IndepHash},
+				{txs, {array, lists:map(fun tx_to_json/1, Txs) }},
+				{hash_list, Hash_list},
+				{wallet_list, Wallet_list}
+			]
+		},
+	json2:encode(EncodedB).
 
 %% @doc Translate fields parsed json from HTTP request into a block
 json_to_block(Json) ->
 	case json2:decode_string(Json) of
 		{done, {ok, Block} , _} ->
-			DecodedBlock =
-				Block#block {
-					nonce = base64:decode(Block#block.nonce),
-					txs = lists:map(fun json_to_tx/1, Block#block.txs)
-				},
-			DecodedBlock;
+			Block#block {
+				nonce = base64:decode(Block#block.nonce),
+				txs = lists:map(fun json_to_tx/1, Block#block.txs)
+			};
 		{_, {error, Reason}, _} ->
 			ar:report([{json_error, Reason}])
 	end.
 
 %% @doc Translate a transaction into json for HTTP
 tx_to_json(
-		#tx {
-			id = ID,
-			owner = Owner,
-			tags = Tags,
-			target = Target,
-			quantity = Quantity,
-			type = Type,
-			data = Data,
-			signature = Sig
-		}) ->
+	#tx {
+		id = ID,
+		owner = Owner,
+		tags = Tags,
+		target = Target,
+		quantity = Quantity,
+		type = Type,
+		data = Data,
+		signature = Sig
+	}) ->
 	EncodedTx =
-			{struct,
-				[
-					{id, ID},
-					{owner, Owner},
-					{tags, Tags},
-					{target, Target},
-					{quantity, Quantity},
-					{type, Type},
-					{data, base64:encode(Data)},
-					{signature, Sig}
-				]
-			},
+		{struct,
+			[
+				{id, ID},
+				{owner, Owner},
+				{tags, {array, Tags}},
+				{target, Target},
+				{quantity, Quantity},
+				{type, Type},
+				{data, base64:encode_to_string(Data)},
+				{signature, Sig}
+			]
+		},
 	json2:encode(EncodedTx).
 
 %% @doc Translate parsed json from fields to a transaction
 json_to_tx(Json) ->
 	case json2:decode_string(Json) of
 		{ok,{struct, TX}} ->
-			DecodedTx =
-				#tx { data = base64:decode(TX#tx.data) },
-			DecodedTx;
+			#tx { data = base64:decode(TX#tx.data) };
 		{_, {error, Reason}, _} ->
 			ar:report([{json_error, Reason}])
 	end.
 
+%% @doc Convert a new block into JSON and back, ensure the result is the same.
 block_roundtrip_test() ->
-	B = ar_weave:init(),
+	[B] = ar_weave:init(),
 	JsonB = block_to_json(B),
 	B1 = json_to_block(JsonB),
 	B = B1.
 
+%% @doc Convert a new TX into JSON and back, ensure the result is the same.
 tx_roundtrip_test() ->
 	TX = ar_tx:new(<<"TEST">>),
 	JsonTX = tx_to_json(TX),
