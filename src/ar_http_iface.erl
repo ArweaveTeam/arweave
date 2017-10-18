4eef7bf22d9610d129fc624ff0c7e100d60722c5
Sam Williams <sam@archain.org>
2017-10-18 19:30:25 +0100

    - Added functions that abstract new block/tx sending.

--- src/ar_http_iface.erl
+++ src/ar_http_iface.erl
@@ -1,5 +1,6 @@
 -module(ar_http_iface).
 -export([start/0, start/1, handle/2, handle_event/3]).
+-export([send_new_block/3, send_new_tx/2]).
 -include("ar.hrl").
 -include("../lib/elli/include/elli.hrl").
 -include_lib("eunit/include/eunit.hrl").
@@ -11,6 +12,8 @@ start() -> start(?DEFAULT_HTTP_IFACE_PORT).
 start(Port) ->
 	{ok, PID} = elli:start_link([{callback, ?MODULE}, {port, Port}]),
 	PID.
+
+%%% Server side functions.
 
 %% @doc Handle a request to the server.
 handle(Req, _Args) ->
@@ -43,24 +46,61 @@ handle_event(Event, Data, Args) ->
 	ar:report([{elli_event, Event}, {data, Data}, {args, Args}]),
 	ok.
 
+%%% Client functions
+
+%% @doc Send a new transaction to an Archain HTTP node.
+send_new_tx(Host, TX) ->
+	httpc:request(
+		post,
+		{
+			"http://" ++ format_host(Host) ++ "/api/add_tx",
+			[],
+			"application/x-www-form-urlencoded",
+			ar_serialize:jsonify(ar_serialize:tx_to_json_struct(TX))
+		}, [], []
+	).
+
+%% @doc Distribute a newly found block to remote nodes.
+send_new_block(Host, NewB, RecallB) ->
+	httpc:request(
+		post,
+		{
+			"http://" ++ format_host(Host) ++ "/api/add_block",
+			[],
+			"application/x-www-form-urlencoded",
+			lists:flatten(
+				ar_serialize:jsonify(
+					{struct,
+						[
+							{new_block,
+								ar_serialize:block_to_json_struct(NewB)},
+							{recall_block,
+								ar_serialize:block_to_json_struct(RecallB)}
+						]
+					}
+				)
+			)
+		}, [], []
+	).
+
+%% @doc Take a remote host ID in various formats, return a HTTP-friendly string.
+format_host({A, B, C, D}) ->
+	format_host({A, B, C, D, ?DEFAULT_HTTP_IFACE_PORT});
+format_host({A, B, C, D, Port}) ->
+	lists:flatten(io_lib:format("~w.~w.~w.~w:~w", [A, B, C, D, Port]));
+format_host(Host) when is_list(Host) ->
+	format_host({Host, ?DEFAULT_HTTP_IFACE_PORT});
+format_host({Host, Port}) ->
+	lists:flatten(io_lib:format("~s:~w", [Host, Port])).
+
+%%% Tests
+
 %% @doc Test adding transactions to a block.
 add_external_tx_test() ->
 	[B0] = ar_weave:init(),
 	Node = ar_node:start([], [B0]),
 	reregister(Node),
-	TX = ar_tx:new(<<"DATA">>),
-	TXJSON = ar_serialize:jsonify(ar_serialize:tx_to_json_struct(TX)),
-	httpc:request(
-		post,
-		{
-			"http://127.0.0.1:"
-				++ integer_to_list(?DEFAULT_HTTP_IFACE_PORT)
-				++ "/api/add_tx",
-			[],
-			"application/x-www-form-urlencoded",
-			TXJSON
-		}, [], []
-	),
+	send_new_tx({127, 0, 0, 1}, TX = ar_tx:new(<<"DATA">>)),
 	receive after 1000 -> ok end,
 	ar_node:mine(Node),
 	receive after 1000 -> ok end,
@@ -77,29 +117,8 @@ add_external_block_test() ->
 	ar_node:mine(Node2),
 	receive after 1000 -> ok end,
 	[B1|_] = ar_node:get_blocks(Node2),
-	JSONB0 = ar_serialize:block_to_json_struct(B0),
-	JSONB1 = ar_serialize:block_to_json_struct(B1),
-	httpc:request(
-		post,
-		{
-			"http://127.0.0.1:"
-				++ integer_to_list(?DEFAULT_HTTP_IFACE_PORT)
-				++ "/api/add_block",
-			[],
-			"application/x-www-form-urlencoded",
-			lists:flatten(
-				ar_serialize:jsonify(
-					{struct,
-						[
-							{new_block, JSONB1},
-							{recall_block, JSONB0}
-						]
-					}
-				)
-			)
-		}, [], []
-	),
-	receive after 1000 -> ok end,
+	send_new_block({127, 0, 0, 1}, B1, B0),
+	receive after 500 -> ok end,
 	[B1, B0] = ar_node:get_blocks(Node1).
 
 %% @doc Helper function : registers a new node as the entrypoint.
