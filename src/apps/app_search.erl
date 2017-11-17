20a6d70350d92c621d116bff3f0f3db6b8777601
william <will@archain.org>
2017-11-17 17:43:58 +0000

    - Merged in app_search changes.

--- src/apps/app_search.erl
+++ src/apps/app_search.erl
@@ -1,5 +1,5 @@
 -module(app_search).
--export([start/0, start/1]).
+-export([start/0, start/1,find_block/2]).
 -export([new_block/2, message/2]).
 -export([build_list/0]).
 -include("ar.hrl").
@@ -10,58 +10,95 @@
 %%% block containing the transaction.
 %%% For examplary purposes only.
 
-%% For compatibility. Dets database supercedes state.
+%% txlist DETS file  
+-define(TXDATA, "data/txlist.file").
+%% @doc For compatibility. Dets database supercedes state.
 -record(state, {
 	db = [] % Stores the 'database' of links to chirps.
 }).
 
 
-%% Start a search node.
+%% @doc Start a search node.
 start() -> start([]).
 start(Peers) ->
-	filelib:ensure_dir("src/apps/tx/"),
+	filelib:ensure_dir(?TXDATA),
 	adt_simple:start(?MODULE, #state{}, Peers).
 	
+find_block(PID, TXID) ->
+	PID ! {get_tx, self(), TXID},
+	receive
+		{found_block, IndepHash} ->
+			IndepHash
+	end.
 
-%% Add the transactions in a newly recieved block to the trasction db
+%% @doc Add the transactions in a newly recieved block to the trasction db
 %% S - current state, B - new block
 new_block(S,B) ->
-	{ok,Ref} = dets:open_file("src/apps/tx/txlist.file",[]),
+	{ok,Ref} = dets:open_file(?TXDATA,[]),
 	{ok,Ref} = insert_transactions(Ref, B#block.txs, B#block.indep_hash),
 	dets:close(Ref),
 	S.
 
-%% Listen for get_transaction requests, send independant block hash
+%% @doc Listen for get_transaction requests, send independant block hash
 %% Back to requesting process.
 %% S - current state, {get_transaction - atom, T - transaction hash,
 %% Process_id - id of the process to return the block hash to}
-message(S,{get_transaction,T,Process_id}) ->
-	{ok,Ref} = dets:open_file("src/apps/tx/txlist.file",[]),
-	Out = dets:lookup(Ref, T),
+message(S,{get_tx, Process_id, T}) ->
+	{ok,Ref} = dets:open_file(?TXDATA,[]),
+	[{_, B_Out}] = dets:lookup(Ref, T),
 	dets:close(Ref),
-	Process_id ! Out,
+	Process_id ! {found_block, B_Out},
 	S;
 message(S, _) ->
 	S.
 
-%% Add to the dets database all transactions from currently held blocks
+%% @doc Add to the dets database all transactions from currently held blocks
 build_list() ->
 	{ok, Filenames} = file:list_dir("blocks"),
-	lists:foreach(fun(Filename) -> 
-						{ok, Binary} = file:read_file("blocks/" ++ Filename),
-						Block = ar_serialize:json_struct_to_block(binary_to_list(Binary)),
-						new_block(new, Block)
-					end,
-					Filenames
-				).
+	lists:foreach(
+		fun(Filename) -> 
+			{ok, Binary} = file:read_file("blocks/" ++ Filename),
+			Block = ar_serialize:json_struct_to_block(binary_to_list(Binary)),
+			new_block(new, Block)
+		end,
+		Filenames
+	).
 
-%% Insert transactions into the dets database
+%% @doc Insert transactions into the dets database
 %% Ref - Ref to dets DB, [T - Transaction hash, Txlist - List of transactions],
 %% Bhash - Independant block hash
 insert_transactions(Ref, [], _) -> 
 	{ok, Ref};
 insert_transactions(Ref, [T|Txlist], Bhash) ->
-	io:format("Txs ~p~n",[Txlist]),
 	dets:insert(Ref, {T#tx.id,Bhash}),
 	insert_transactions(Ref, Txlist, Bhash).
 
+%% @doc Test that a new tx placed on the network and mined can be searched for
+basic_usage_test() ->
+	% Spawn a network with two nodes and a chirper server
+	ar_storage:clear(),
+	SearchServer = start(),
+	Peers = ar_network:start(10, 10),
+	ar_node:add_peers(hd(Peers), SearchServer),
+	% Create the transaction, send it.
+	{Priv, Pub} = ar_wallet:new(),
+	% Generate the transaction.
+	TX = ar_tx:new(<<"Hello">>),
+	% Sign the TX with the public and private key.
+	SignedTX = ar_tx:sign(TX, Priv, Pub),
+	% Add tx to network
+	ar_node:add_tx(hd(Peers), SignedTX),
+	% Begin mining
+	receive after 250 -> ok end,
+	ar_node:mine(hd(Peers)),
+	receive after 1000 -> ok end,
+	{ok,Ref} = dets:open_file(?TXDATA,[]),
+	% recieve a "get transaction" message
+	message([], {get_transaction,self(),TX#tx.id}),
+	% check that newly mined block matches the block the most recent transaction was mined in 
+	receive
+		X ->
+			X = (hd(ar_node:get_blocks(hd(Peers))))#block.indep_hash	
+	end,
+	dets:close(Ref).
+	
