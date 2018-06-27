351b21b7d70279299cf6bee7f83e1e36a43da1ef
Damon Sweeney <damon@arweave.org>
2018-06-27 12:14:29 +0100

    - Cleaned up ar_node state definition.

--- src/ar_node.erl
+++ src/ar_node.erl
@@ -54,25 +54,33 @@
 
 %%% Blockweave maintaining nodes in the Arweave system.
 
+% NB: The potential_txs field stores the set of txs that contradict those
+% already stored within the txs field. That is those that have the same last
+% tx reference as one already held in txs.
+%
+% These txs are not dropped as when a block is a mined blockshadows do not
+% redistribute the set mined on, only their IDs. As such they may be required
+% to validate newly distributed blocks from other nodes in the network.
+%
 -record(state, {
-	hash_list = not_joined,
-	wallet_list = [],
-	floating_wallet_list = [],
-	height = 0,
-	gossip,
-	txs = [],
-	miner,
-	mining_delay = 0,
-	automine = false,
-	reward_addr = unclaimed,
-	trusted_peers = [],
-	waiting_txs = [],
-	potential_txs = [],
-    tags = [],
-	reward_pool = 0,
-	diff = 0,
-	last_retarget,
-	weave_size = 0
+	hash_list = not_joined, % current full hashlist
+	wallet_list = [], % current up to date walletlist
+	floating_wallet_list = [], % up to date walletlist with new txs applied
+	height = 0, % current height of the blockweave
+	gossip, % Gossip protcol state
+	txs = [], % set of new txs to be mined into the next block
+	miner, % PID of the mining process
+	mining_delay = 0, % delay on mining, used for netework simulation
+	automine = false, % boolean dictating if a node should automine
+	reward_addr = unclaimed, % reward address for mining a new block
+	trusted_peers = [], % set of trusted peers used to join on
+	waiting_txs = [], % set of txs on timeout whilst network distribution occurs
+	potential_txs = [], % set of valid but contradictory txs
+    tags = [], % nodes tags to apply to a block when mining
+	reward_pool = 0, % current mining rewardpool of the weave
+	diff = 0, % current mining difficulty of the weave (no. of preceeding zero)
+	last_retarget, % timestamp at which the last difficulty retarget occurred
+	weave_size = 0 % current size of the weave in bytes (only inc. data tx size)
 }).
 
 %% Maximum number of blocks to hold at any time.
