b453934c69c9f63a4c683458f5619f57051050d4
Sam Williams <samuel.ecwilliams@gmail.com>
2018-08-11 18:48:59 +0200

    - Don't gossip blocks below or equal to your height.

--- src/ar_http_iface.erl
+++ src/ar_http_iface.erl
@@ -202,7 +202,7 @@ handle('POST', [<<"block">>], Req) ->
 							CurrentB = ar_node:get_current_block(whereis(http_entrypoint_node)),
 							% mue: keep block distance for later tests
 							case (not is_atom(CurrentB)) andalso
-								(B#block.height >= CurrentB#block.height) andalso 
+								(B#block.height > CurrentB#block.height) andalso 
 								(B#block.height =< (CurrentB#block.height + 50)) andalso
 								(B#block.diff >= ?MIN_DIFF) of
 								true ->
