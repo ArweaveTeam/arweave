15fc76712dd7f0ac96bdeeecd9e7109877f1469f
Sam Williams <samuel.ecwilliams@gmail.com>
2018-10-05 13:38:43 -0700

    - Fixed getting peer issue, as reported by Mike.

--- src/ar_node_worker.erl
+++ src/ar_node_worker.erl
@@ -339,7 +339,7 @@ maybe_remove_tx(TXs, TXID, Sig) ->
 %% @doc Validate whether a new block is legitimate, then handle it, optionally
 %% dropping or starting a fork recoverer as appropriate.
 process_new_block(_StateIn, NewGS, NewB, _, _Peer, not_joined) ->
-	ar_join:start(ar_gossip:peers(NewGS, NewB)),
+	ar_join:start(ar_gossip:peers(NewGS)),
 	none;
 process_new_block(#{ height := Height } = StateIn, NewGS, NewB, unavailable, Peer, HashList)
 		when NewB#block.height == Height + 1 ->
