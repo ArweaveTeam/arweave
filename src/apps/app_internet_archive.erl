1993f027fe0927771b451d51717c54917e95f179
Sam Williams <samuel.ecwilliams@gmail.com>
2018-10-05 18:14:06 -0700

    - Added item parser from JSON.

--- src/apps/app_internet_archive.erl
+++ src/apps/app_internet_archive.erl
@@ -11,7 +11,6 @@
 %% Represents an item from the IA to be stored inside the Arweave.
 -record(item, {
     id,
-    hash,
     meta_data = [],
     torrent = <<>>
 }).
@@ -38,7 +37,6 @@ item_to_tx(I) ->
             [
                 {"app_name", "InternetArchive"},
                 {"id", I#item.id},
-                {"hash", ar_util:encode(I#item.hash)},
                 {"Content-Type", "application/x-bittorrent"}
             ] ++ I#item.meta_data,
         data =
@@ -59,6 +57,52 @@ item_to_tx(I) ->
             end
     }.
 
+%% @doc Get all items from a collection.
+%get_collection(CollectionID) when is_binary(CollectionID) ->
+%    get_collection(binary_to_list(CollectionID));
+%get_collection(CollectionID) ->
+%    proplists:get_value(<<"">>, get_url(a)).
+
+%% @doc When given an ID, build an item from it using the API.
+id_to_item(ItemID)  when is_binary(ItemID) -> id_to_item(binary_to_list(ItemID));
+id_to_item(ItemID) ->
+    json_to_item(
+        ar_serialize:dejsonify(get_url("https://archive.org/metadata/" ++ ItemID))
+    ).
+
+%% @doc Parse a JSONStruct into an #item{}.
+json_to_item({JSON}) ->
+    {Metadata} = proplists:get_value(<<"metadata">>, JSON),
+    Files = proplists:get_value(<<"files">>, JSON),
+    ID = proplists:get_value(<<"identifier">>, Metadata),
+    [Torrent|_] = lists:filtermap(fun is_torrent_file/1, Files),
+    #item {
+        id = ID,
+        meta_data = Metadata,
+        torrent =
+            list_to_binary(
+                get_url(
+                    "https://archive.org/download/"
+                        ++ binary_to_list(ID)
+                        ++ "/"
+                        ++ binary_to_list(Torrent)
+                )
+            )
+    }.
+
+%% @doc Determine whether the given JSONStruct representation of a file contains
+%% a torrent file. If it does, return the file name. If it does not, return false.
+is_torrent_file({File}) ->
+    case re:run(Name = proplists:get_value(<<"name">>, File), ".+\.torrent$") of
+        nomatch -> false;
+        _ -> {true, Name}
+    end.
+
+%% @doc Get a page, assuming 200 response (crashing if not).
+get_url(URL) ->
+    {ok, {{_, 200, _}, _, Body}} = httpc:request(URL),
+    Body.
+
 %% @doc Generate a list of items to store in the Arweave.
 %% For the moment, this is static test data, until we have a way to dynamically
 %% extract it.
@@ -67,7 +111,6 @@ generate_items() ->
     [
         #item {
             id = "NineteenEightyFour1984ByGeorgeOrwell",
-            hash = crypto:strong_rand_bytes(32),
             torrent =
                 "https://archive.org/download"
                     "/NineteenEightyFour1984ByGeorgeOrwell"
