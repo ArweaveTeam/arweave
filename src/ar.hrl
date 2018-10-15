208342eede79b53df898b77f14c8d6328fe1a9b0
Sam Williams <samuel.ecwilliams@gmail.com>
2018-10-15 17:07:15 +0200

    - Bumped release number.

--- src/ar.hrl
+++ src/ar.hrl
@@ -7,7 +7,7 @@
 -define(CLIENT_VERSION, 3).
 
 %% @doc The current build number -- incremented for every release.
--define(RELEASE_NUMBER, 8).
+-define(RELEASE_NUMBER, 9).
 
 -define(DEFAULT_REQUEST_HEADERS,
 	[
