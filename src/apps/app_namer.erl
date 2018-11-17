552ef41b5bba4b1443a9bb215370e536f716988e
Sam Williams <samuel.ecwilliams@gmail.com>
2018-11-17 21:36:52 +0100

    Added support for different matching functions.

--- src/apps/app_namer.erl
+++ src/apps/app_namer.erl
@@ -1,5 +1,5 @@
 -module(app_namer).
--export([start/1, start/2, stop/1]).
+-export([start/1, start/2, start/3, stop/1]).
 
 %%% Generate random wallets until you find one that starts with the given 
 %%% characters.
@@ -7,20 +7,21 @@
 %% @doc Start a new name searcher, looking for a wallet that starts with the
 %% given characters. Optionally takes a number of cores to search with.
 start(Str) -> start(Str, 1).
-start(Str, Cores) ->
-    [ spawn(fun() -> server(Str) end) || _ <- lists:seq(1, Cores) ].
+start(Str, Cores) -> start(Str, Cores, normal).
+start(Str, Cores, Type) ->
+    [ spawn(fun() -> server(Str, Type) end) || _ <- lists:seq(1, Cores) ].
 
 %% @doc Stops all of the processes in a name search.
 stop(PIDs) ->
     lists:foreach(fun(PID) -> PID ! stop end, PIDs).
 
 %% @doc Main server body. Check for stop, then make a guess.
-server(Str) ->
+server(Type, Str) ->
     receive
         stop -> ok
     after 0 ->
-        maybe_save(Str, guess()),
-        server(Str)
+        maybe_save(Type, Str, guess()),
+        server(Type, Str)
     end.
 
 %% @doc Generate a new wallet and its address.
@@ -34,10 +35,14 @@ guess() ->
     ).
 
 %% @doc If the wallet is a hit, save it and notify the user.
-maybe_save(Str, Addr) ->
-    case lists:prefix(string:to_lower(Str), string:to_lower(Addr)) of
+maybe_save(Type, Str, Addr) ->
+    case matches(Type, Str, Addr) of
         false ->
             file:delete("wallets/arweave_keyfile_" ++ Addr ++ ".json");
         true ->
             io:format("Found wallet ~s!~n", [Addr])
     end.
+
+%% @doc Check whether an address matches a query.
+matches(normal, Str, Addr) ->
+    lists:prefix(string:to_lower(Str), string:to_lower(Addr)).
