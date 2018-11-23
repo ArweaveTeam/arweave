f0583d82454d18cc1d8bf62c2b6554484baca162
Lev Berman <ldmberman@gmail.com>
2018-11-23 16:07:58 +0100

    Check integrity of the downloaded Erlang tarball

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ echo "-----INSTALLING DEPENDENCIES-----"
 sudo apt-get update
 sudo apt-get install -y gcc make build-essential autoconf m4 libncurses5-dev libssh-dev unixodbc-dev openjdk-8-jdk libwxgtk3.0-dev xsltproc fop
 wget http://www.erlang.org/download/otp_src_20.1.tar.gz
+if [ `shasum -a 256 otp_src_20.1.tar.gz | awk {'print $1'}` != 900d35eb563607785a8e27f4b4c03cf6c98b4596028c5d6958569ddde5d4ddbf ]; then
+	echo 'Integrity check failed for otp_src_20.1.tar.gz. Remove the file and try to download it from another source.'
+	exit 1
+fi
 tar -zvxf otp_src_20.1.tar.gz
 cd otp_src_20.1
 export ERL_TOP=`pwd`
