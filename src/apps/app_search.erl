1e539340640d4ea6f17651408b555ce0ab2af7c9
Lev Berman <ldmberman@gmail.com>
2019-01-04 12:17:12 +0100

    Attack block_indep_hash system tag in app_search

--- src/apps/app_search.erl
+++ src/apps/app_search.erl
@@ -72,6 +72,7 @@ update_tag_table(B) when ?IS_BLOCK(B) ->
 						{<<"to">>, _} -> ok;
 						{<<"quantity">>, _} -> ok;
 						{<<"reward">>, _} -> ok;
+						{<<"block_indep_hash">>, _} -> ok;
 						{Name, Value} -> add_entry(Name, Value, TX#tx.id);
 						_ -> ok
 					end
@@ -81,7 +82,8 @@ update_tag_table(B) when ?IS_BLOCK(B) ->
 			add_entry(<<"from">>, ar_util:encode(ar_wallet:to_address(TX#tx.owner)), TX#tx.id),
 			add_entry(<<"to">>, ar_util:encode(ar_wallet:to_address(TX#tx.target)), TX#tx.id),
 			add_entry(<<"quantity">>, TX#tx.quantity, TX#tx.id),
-			add_entry(<<"reward">>, TX#tx.reward, TX#tx.id)
+			add_entry(<<"reward">>, TX#tx.reward, TX#tx.id),
+			add_entry(<<"block_indep_hash">>, ar_util:encode(B#block.indep_hash), TX#tx.id)
 		end,
 		ar_storage:read_tx(B#block.txs)
 	);
