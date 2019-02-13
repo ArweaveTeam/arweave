fbe21ea6b3ea70fca304c52c512609756a698801
Ivan Uemlianin <ivan@llaisdy.com>
2019-02-13 17:21:51 +0000

    ipfsar.sh runs periodically (default sleep 1h).

--- doc/ipfsar.sh
+++ doc/ipfsar.sh
@@ -3,15 +3,16 @@
 APIKEY="test_key_ivan"
 SERVER="http://178.62.126.142:1984"
 PINS_DONE_FN="pins.done"
+SLEEP=1h
 
 curlpost () {  # $1=hash to pin
 	echoerr $1
 	path="/api/ipfs/getsend/"
 	url="$SERVER$path"
 	json="{\"api_key\":\"$APIKEY\", \"ipfs_hash\":\"$1\"}"
-	echoerr $json
+	#echoerr $json
 	/usr/bin/curl -H "Content-Type: application/json" -X POST -d "$json" $url
-	sleep .5s
+	sleep .125s
 	echo 200
 }
 
@@ -19,25 +20,28 @@ echoerr () {
 	printf "%s\n" "$*" >&2
 }
 
-#mapfile -t pins_done < $PINS_DONE_FN # linux / current bash
-while IFS= read -r line; do pins_done+=("$line"); done < $PINS_DONE_FN # mac / old bash
+while [ true ]
+do
 
-pins_done_sorted=($(sort <<<"${pins_done[*]}"))
+  #mapfile -t pins_done < $PINS_DONE_FN # linux / current bash
+  while IFS= read -r line; do pins_done+=("$line"); done < $PINS_DONE_FN # mac / old bash
 
-pins_curr=`ipfs pin ls | sort | awk '{print $1}'`
-pins_curr_sorted=($(sort <<<"${pins_curr[*]}"))
+  pins_done_sorted=($(sort <<<"${pins_done[*]}"))
 
-# https://stackoverflow.com/questions/2312762/compare-difference-of-two-arrays-in-bash
-pins_new=()
-for i in "${pins_curr_sorted[@]}"; do
+  pins_curr=`ipfs pin ls | sort | awk '{print $1}'`
+  pins_curr_sorted=($(sort <<<"${pins_curr[*]}"))
+
+  # https://stackoverflow.com/questions/2312762/compare-difference-of-two-arrays-in-bash
+  pins_new=()
+  for i in "${pins_curr_sorted[@]}"; do
 	skip=
 	for j in "${pins_done_sorted[@]}"; do
 		[[ $i == $j ]] && { skip=1; break; }
 	done
 	[[ -n $skip ]] || pins_new+=("$i")
-done
+  done
 
-for p in "${pins_new[@]}"; do
+  for p in "${pins_new[@]}"; do
 	response=$(curlpost $p)
 	case $response in # 200 208 402
 		200)
@@ -46,11 +50,19 @@ for p in "${pins_new[@]}"; do
 		208)
 			pins_done+=("$p")
 			;;
-		402)
+		*)
 			break # ??
 	esac
-done
+  done
 
-#printf "%s\n" "${pins_new[@]}" > pins.new
-#printf "%s\n" "${pins_curr_sorted[@]}" > pins.curr
-printf "%s\n" "${pins_done[@]}" > $PINS_DONE_FN
+  #printf "%s\n" "${pins_new[@]}" > pins.new
+  #printf "%s\n" "${pins_done_sorted[@]}" > pins.done_sorted
+  #printf "%s\n" "${pins_curr_sorted[@]}" > pins.curr
+  printf "%s\n" "${pins_done[@]}" > $PINS_DONE_FN
+  pins_done=()
+
+  #echoerr "Sleeping ..."
+  sleep $SLEEP
+  #echoerr "...    Slept"
+
+done
