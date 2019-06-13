9675d84a620e32cffa8156a10a2614785ab364b3
Lev Berman <ldmberman@protonmail.com>
2019-06-13 19:13:16 +0200

    RandomX NIF: Use Dirty schedulers instead of threads
    Make each NIF call Dirty (see "Dirty NIF" in
    http://erlang.org/doc/man/erl_nif.html) instead of doing the heavy
    work in a thread. This simplifies the code while mostly keeping the
    same behavior (and performance).
    
    There is only one dirty scheduler per regular scheduler. If all the
    dirty schedulers are busy, then the next Dirty NIF call will be
    blocked. This will cause a delay of hashing if running on a
    single-core machine and the only dirty scheduler is busy generating a
    dataset. This could delay verifying new blocks received but should be
    fine.
    
    Co-authored-by: Martin Torhage <martin@arweave.org>

---
+++ c_src/ar_mine_randomx.h
@@ -0,0 +1,46 @@
+#include "randomx.h"
+
+typedef enum { FALSE, TRUE } boolean;
+
+typedef enum {
+	HASHING_MODE_FAST = 0,
+	HASHING_MODE_LIGHT = 1,
+} hashing_mode;
+
+struct workerThread {
+	ErlNifTid threadId;
+	ErlNifThreadOpts *optsPtr;
+	randomx_cache *cachePtr;
+	randomx_dataset *datasetPtr;
+	unsigned long datasetInitStartItem;
+	unsigned long datasetInitItemCount;
+};
+
+struct state {
+	ErlNifRWLock*     lockPtr;
+	int               isRandomxReleased;
+	randomx_dataset*  datasetPtr;
+	randomx_cache*    cachePtr;
+};
+
+static int load(ErlNifEnv*, void**, ERL_NIF_TERM);
+static void state_dtor(ErlNifEnv*, void*);
+static void release_randomx(struct state*);
+
+static ERL_NIF_TERM init_fast_nif(ErlNifEnv*, int, const ERL_NIF_TERM []);
+static ERL_NIF_TERM init_light_nif(ErlNifEnv*, int, const ERL_NIF_TERM []);
+static ERL_NIF_TERM init(ErlNifEnv*, int, const ERL_NIF_TERM [], hashing_mode);
+static boolean init_dataset(randomx_dataset*, randomx_cache*, unsigned int);
+static void *init_dataset_thread(void*);
+static ERL_NIF_TERM init_failed(ErlNifEnv*, struct state*, const char*);
+
+static ERL_NIF_TERM hash_fast_nif(ErlNifEnv*, int, const ERL_NIF_TERM []);
+static ERL_NIF_TERM hash_light_nif(ErlNifEnv*, int, const ERL_NIF_TERM []);
+static ERL_NIF_TERM hash_nif(ErlNifEnv*, int, const ERL_NIF_TERM [], hashing_mode);
+
+static ERL_NIF_TERM release_state_nif(ErlNifEnv*, int, const ERL_NIF_TERM []);
+
+static ERL_NIF_TERM ok_tuple(ErlNifEnv*, ERL_NIF_TERM);
+static ERL_NIF_TERM error_tuple(ErlNifEnv*, ERL_NIF_TERM);
+static ERL_NIF_TERM error(ErlNifEnv*, const char*);
+static ERL_NIF_TERM make_output_binary(ErlNifEnv*, char*, size_t);
