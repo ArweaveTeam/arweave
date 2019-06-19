b8c5a395a40d2680870af8c51fcb77ccbb2ed84e
Martin Torhage <martin@arweave.org>
2019-06-19 11:24:33 +0200

    app_ipfs_tests: Comment out unused code

--- src/apps/app_ipfs_tests.erl
+++ src/apps/app_ipfs_tests.erl
@@ -53,82 +53,82 @@ ar_node_init() ->
 	Pid = ar_node:start([], B0),
 	Pid.
 
-mine_n_blocks_on_node(N, Node) ->
-	lists:foreach(fun(_) ->
-			ar_node:mine(Node),
-			timer:sleep(1000)
-		end, lists:seq(1,N)),
-	timer:sleep(1000),
-	ar_node:get_blocks(Node).
+% mine_n_blocks_on_node(N, Node) ->
+% 	lists:foreach(fun(_) ->
+% 			ar_node:mine(Node),
+% 			timer:sleep(1000)
+% 		end, lists:seq(1,N)),
+% 	timer:sleep(1000),
+% 	ar_node:get_blocks(Node).
 
-add_n_txs_to_node(N, Node) ->
-	% cribbed from ar_http_iface:add_external_tx_with_tags_test/0.
-	prepare_tx_adder(Node),
-	lists:map(fun(_) ->
-			Tags = [
-				{<<"TEST_TAG1">>, <<"TEST_VAL1">>},
-				{<<"TEST_TAG2">>, <<"TEST_VAL2">>}
-			],
-			TX = tag_tx(ar_tx:new(<<"DATA">>), Tags),
-			ar_http_iface:send_new_tx({127, 0, 0, 1, 1984}, TX),
-			receive after 1000 -> ok end,
-			ar_node:mine(Node),
-			receive after 1000 -> ok end,
-			TX#tx.id
-		end,
-		lists:seq(1,N)).
+% add_n_txs_to_node(N, Node) ->
+% 	% cribbed from ar_http_iface:add_external_tx_with_tags_test/0.
+% 	prepare_tx_adder(Node),
+% 	lists:map(fun(_) ->
+% 			Tags = [
+% 				{<<"TEST_TAG1">>, <<"TEST_VAL1">>},
+% 				{<<"TEST_TAG2">>, <<"TEST_VAL2">>}
+% 			],
+% 			TX = tag_tx(ar_tx:new(<<"DATA">>), Tags),
+% 			ar_http_iface:send_new_tx({127, 0, 0, 1, 1984}, TX),
+% 			receive after 1000 -> ok end,
+% 			ar_node:mine(Node),
+% 			receive after 1000 -> ok end,
+% 			TX#tx.id
+% 		end,
+% 		lists:seq(1,N)).
 
-add_n_tx_pairs_to_node(N, Node) ->
-	prepare_tx_adder(Node),
-	BoringTags = [
-		{<<"TEST_TAG1">>, <<"TEST_VAL1">>},
-		{<<"TEST_TAG2">>, <<"TEST_VAL2">>}
-	],
-	lists:map(fun(X) ->
-			TX1 = tag_tx(ar_tx:new(timestamp_data(<<"DATA">>)), BoringTags),
-			send_tx_mine_block(Node, TX1),
-			TS = ts_bin(),
-			Filename = numbered_fn(X),
-			Data = timestamp_data(TS, <<"Data">>),
-			Tags = [{<<"IPFS-Add">>, Filename}],
-			TX2 = tag_tx(ar_tx:new(Data), Tags),
-			send_tx_mine_block(Node, TX2),
-			TS
-		end,
-		lists:seq(1,N)).
+% add_n_tx_pairs_to_node(N, Node) ->
+% 	prepare_tx_adder(Node),
+% 	BoringTags = [
+% 		{<<"TEST_TAG1">>, <<"TEST_VAL1">>},
+% 		{<<"TEST_TAG2">>, <<"TEST_VAL2">>}
+% 	],
+% 	lists:map(fun(X) ->
+% 			TX1 = tag_tx(ar_tx:new(timestamp_data(<<"DATA">>)), BoringTags),
+% 			send_tx_mine_block(Node, TX1),
+% 			TS = ts_bin(),
+% 			Filename = numbered_fn(X),
+% 			Data = timestamp_data(TS, <<"Data">>),
+% 			Tags = [{<<"IPFS-Add">>, Filename}],
+% 			TX2 = tag_tx(ar_tx:new(Data), Tags),
+% 			send_tx_mine_block(Node, TX2),
+% 			TS
+% 		end,
+% 		lists:seq(1,N)).
 
-ipfs_hashes_to_data(Pid) ->
-	lists:map(fun(Hash) ->
-				{ok, Data} = ar_ipfs:cat_data_by_hash(Hash),
-				Data
-		end,
-		lists:reverse(app_ipfs:get_ipfs_hashes(Pid))).
+% ipfs_hashes_to_data(Pid) ->
+% 	lists:map(fun(Hash) ->
+% 				{ok, Data} = ar_ipfs:cat_data_by_hash(Hash),
+% 				Data
+% 		end,
+% 		lists:reverse(app_ipfs:get_ipfs_hashes(Pid))).
 
-ipfs_hashes_to_TSs(Pid) ->
-	lists:map(fun(Bin) ->
-				<<TS:25/binary,_/binary>> = Bin,
-				TS
-		end,
-		ipfs_hashes_to_data(Pid)).
+% ipfs_hashes_to_TSs(Pid) ->
+% 	lists:map(fun(Bin) ->
+% 				<<TS:25/binary,_/binary>> = Bin,
+% 				TS
+% 		end,
+% 		ipfs_hashes_to_data(Pid)).
 
-numbered_fn(N) ->
-	NB = integer_to_binary(N),
-	<<"testdata-", NB/binary, ".txt">>.
+% numbered_fn(N) ->
+% 	NB = integer_to_binary(N),
+% 	<<"testdata-", NB/binary, ".txt">>.
 
-prepare_tx_adder(Node) ->
-	ar_http_iface_server:reregister(Node),
-	Bridge = ar_bridge:start([], [], Node),
-	ar_http_iface_server:reregister(http_bridge_node, Bridge),
-	ar_node:add_peers(Node, Bridge).
+% prepare_tx_adder(Node) ->
+% 	ar_http_iface_server:reregister(Node),
+% 	Bridge = ar_bridge:start([], [], Node),
+% 	ar_http_iface_server:reregister(http_bridge_node, Bridge),
+% 	ar_node:add_peers(Node, Bridge).
 
-send_tx_mine_block(Node, TX) ->
-	ar_http_iface_client:send_new_tx({127, 0, 0, 1, 1984}, TX),
-	receive after 1000 -> ok end,
-	ar_node:mine(Node),
-	receive after 1000 -> ok end.
+% send_tx_mine_block(Node, TX) ->
+% 	ar_http_iface_client:send_new_tx({127, 0, 0, 1, 1984}, TX),
+% 	receive after 1000 -> ok end,
+% 	ar_node:mine(Node),
+% 	receive after 1000 -> ok end.
 
-tag_tx(TX, Tags) ->
-	TX#tx{tags=Tags}.
+% tag_tx(TX, Tags) ->
+% 	TX#tx{tags=Tags}.
 
 timestamp_data(Data) ->
 	timestamp_data(ts_bin(), Data).
@@ -137,4 +137,3 @@ timestamp_data(TS, Data) ->
 
 ts_bin() ->
 	list_to_binary(calendar:system_time_to_rfc3339(erlang:system_time(second))).
-
