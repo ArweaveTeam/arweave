d633fd1985e31f41eb75b98620ce8abe6a0bf38e
Lev Berman <ldmberman@protonmail.com>
2019-09-24 11:25:17 +0200

    Bump RELEASE_NUMBER 27 => 28 for 1.8.2.0

--- src/ar.hrl
+++ src/ar.hrl
@@ -10,7 +10,7 @@
 -define(CLIENT_VERSION, 5).
 
 %% @doc The current build number -- incremented for every release.
--define(RELEASE_NUMBER, 27).
+-define(RELEASE_NUMBER, 28).
 
 -define(DEFAULT_REQUEST_HEADERS,
 	[
