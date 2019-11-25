c659c9e9fffbda85e6202d11e6d9cac7ca42fade
Maximilien Mellen <max@arweave.org>
2019-11-25 14:06:49 +0100

    Replace erlang-sqlite3 with esqlite

--- apps/arweave/src/ar_sqlite3.erl
+++ apps/arweave/src/ar_sqlite3.erl
@@ -11,8 +11,6 @@
 
 -include("ar.hrl").
 
--define(DB_NAME, list_to_atom(atom_to_list(?MODULE) ++ "_db")).
-
 %% The migration name should follow the format YYYYMMDDHHMMSS_human_readable_name
 %% where the date is picked at the time of naming the migration.
 -define(CREATE_MIGRATION_TABLE_SQL, "
@@ -132,17 +130,18 @@ init(Opts) ->
 	ar:info([{ar_sqlite3, init}, {data_dir, DataDir}]),
 	DbPath = filename:join([DataDir, ?SQLITE3_DIR, "arql.db"]),
 	ok = filelib:ensure_dir(DbPath),
-	{ok, _} = sqlite3:open(?DB_NAME, [{file, DbPath}]),
-	ok = ensure_meta_table_created(),
-	ok = ensure_schema_created(),
-	{ok, InsertBlockStmt} = sqlite3:prepare(?DB_NAME, ?INSERT_BLOCK_SQL),
-	{ok, InsertTxStmt} = sqlite3:prepare(?DB_NAME, ?INSERT_TX_SQL),
-	{ok, InsertTagStmt} = sqlite3:prepare(?DB_NAME, ?INSERT_TAG_SQL),
-	{ok, SelectTxByIdStmt} = sqlite3:prepare(?DB_NAME, ?SELECT_TX_BY_ID_SQL),
-	{ok, SelectBlockByTxIdStmt} = sqlite3:prepare(?DB_NAME, ?SELECT_BLOCK_BY_TX_ID_SQL),
-	{ok, SelectTagsByTxIdStmt} = sqlite3:prepare(?DB_NAME, ?SELECT_TAGS_BY_TX_ID_SQL),
+	{ok, Conn} = esqlite3:open(DbPath),
+	ok = ensure_meta_table_created(Conn),
+	ok = ensure_schema_created(Conn),
+	{ok, InsertBlockStmt} = esqlite3:prepare(?INSERT_BLOCK_SQL, Conn),
+	{ok, InsertTxStmt} = esqlite3:prepare(?INSERT_TX_SQL, Conn),
+	{ok, InsertTagStmt} = esqlite3:prepare(?INSERT_TAG_SQL, Conn),
+	{ok, SelectTxByIdStmt} = esqlite3:prepare(?SELECT_TX_BY_ID_SQL, Conn),
+	{ok, SelectBlockByTxIdStmt} = esqlite3:prepare(?SELECT_BLOCK_BY_TX_ID_SQL, Conn),
+	{ok, SelectTagsByTxIdStmt} = esqlite3:prepare(?SELECT_TAGS_BY_TX_ID_SQL, Conn),
 	{ok, #{
 		data_dir => DataDir,
+		conn => Conn,
 		insert_block_stmt => InsertBlockStmt,
 		insert_tx_stmt => InsertTxStmt,
 		insert_tag_stmt => InsertTagStmt,
@@ -153,14 +152,13 @@ init(Opts) ->
 
 handle_call({select_tx_by_id, ID}, _, State) ->
 	#{ select_tx_by_id_stmt := Stmt } = State,
-	ok = sqlite3:bind(?DB_NAME, Stmt, [ID]),
-	Reply = case sqlite3:next(?DB_NAME, Stmt) of
+	ok = esqlite3:bind(Stmt, [ID]),
+	Reply = case esqlite3:fetchone(Stmt) of
 		Tuple when is_tuple(Tuple) -> {ok, tx_map(Tuple)};
-		done -> not_found
+		ok -> not_found
 	end,
-	ok = sqlite3:reset(?DB_NAME, Stmt),
 	{reply, Reply, State};
-handle_call({select_txs_by, Opts}, _, State) ->
+handle_call({select_txs_by, Opts}, _, #{ conn := Conn } = State) ->
 	{WhereClause, Params} = select_txs_by_where_clause(Opts),
 	SQL = lists:concat([
 		"SELECT tx.* FROM tx ",
@@ -168,27 +166,28 @@ handle_call({select_txs_by, Opts}, _, State) ->
 		"WHERE ", WhereClause,
 		" ORDER BY block.height DESC, tx.id DESC"
 	]),
-	{ok, Stmt} = sqlite3:prepare(?DB_NAME, SQL),
-	ok = sqlite3:bind(?DB_NAME, Stmt, Params),
-	Reply = collect_txs(Stmt),
-	ok = sqlite3:finalize(?DB_NAME, Stmt),
+	Reply = case esqlite3:q(SQL, Params, Conn) of
+		Rows when is_list(Rows) ->
+			lists:map(fun tx_map/1, Rows)
+	end,
 	{reply, Reply, State};
 handle_call({select_block_by_tx_id, TXID}, _, State) ->
 	#{ select_block_by_tx_id_stmt := Stmt } = State,
-	ok = sqlite3:bind(?DB_NAME, Stmt, [TXID]),
-	Reply = case sqlite3:next(?DB_NAME, Stmt) of
+	ok = esqlite3:bind(Stmt, [TXID]),
+	Reply = case esqlite3:fetchone(Stmt) of
 		Tuple when is_tuple(Tuple) -> {ok, block_map(Tuple)};
-		done -> not_found
+		ok -> not_found
 	end,
-	ok = sqlite3:reset(?DB_NAME, Stmt),
 	{reply, Reply, State};
 handle_call({select_tags_by_tx_id, TXID}, _, State) ->
 	#{ select_tags_by_tx_id_stmt := Stmt } = State,
-	ok = sqlite3:bind(?DB_NAME, Stmt, [TXID]),
-	Reply = collect_tags(Stmt),
-	ok = sqlite3:reset(?DB_NAME, Stmt),
+	ok = esqlite3:bind(Stmt, [TXID]),
+	Reply = case esqlite3:fetchall(Stmt) of
+		Rows when is_list(Rows) ->
+			lists:map(fun tags_map/1, Rows)
+	end,
 	{reply, Reply, State};
-handle_call({eval_legacy_arql, Query}, _, State) ->
+handle_call({eval_legacy_arql, Query}, _, #{ conn := Conn } = State) ->
 	Reply = case catch eval_legacy_arql_where_clause(Query) of
 		{WhereClause, Params} ->
 			SQL = lists:concat([
@@ -197,15 +196,13 @@ handle_call({eval_legacy_arql, Query}, _, State) ->
 				"WHERE ", WhereClause,
 				" ORDER BY block.height DESC, tx.id DESC"
 			]),
-			case sqlite3:prepare(?DB_NAME, SQL) of
-				{ok, Stmt} ->
-					ok = sqlite3:bind(?DB_NAME, Stmt, Params),
-					R = collect_txids(Stmt),
-					ok = sqlite3:finalize(?DB_NAME, Stmt),
-					R;
-				{error, 1, "parser stack overflow"} ->
+			case catch esqlite3:q(SQL, Params, Conn) of
+				Rows when is_list(Rows) ->
+					lists:map(fun({TXID}) -> TXID end, Rows);
+				{error, {sqlite_error, "parser stack overflow"}} ->
 					sqlite_parser_stack_overflow;
-				_ ->
+				Error ->
+					ar:warn([{ar_sqlite3, eval_legacy_arql}, {error, Error}]),
 					bad_query
 			end;
 		bad_query ->
@@ -218,117 +215,99 @@ handle_cast({populate_db, BHL}, State) ->
 	{noreply, State};
 handle_cast({insert_block, BlockFields}, State) ->
 	#{ insert_block_stmt := Stmt } = State,
-	ok = sqlite3:bind(?DB_NAME, Stmt, BlockFields),
-	done = sqlite3:next(?DB_NAME, Stmt),
-	ok = sqlite3:reset(?DB_NAME, Stmt),
+	ok = esqlite3:bind(Stmt, BlockFields),
+	ok = esqlite3:fetchone(Stmt),
 	{noreply, State};
 handle_cast({insert_tx, TxFields}, State) ->
 	#{ insert_tx_stmt := Stmt } = State,
-	ok = sqlite3:bind(?DB_NAME, Stmt, TxFields),
-	done = sqlite3:next(?DB_NAME, Stmt),
-	ok = sqlite3:reset(?DB_NAME, Stmt),
+	ok = esqlite3:bind(Stmt, TxFields),
+	ok = esqlite3:fetchone(Stmt),
 	{noreply, State};
 handle_cast({insert_tag, TagFields}, State) ->
 	#{ insert_tag_stmt := Stmt } = State,
-	ok = sqlite3:bind(?DB_NAME, Stmt, TagFields),
-	done = sqlite3:next(?DB_NAME, Stmt),
-	ok = sqlite3:reset(?DB_NAME, Stmt),
+	ok = esqlite3:bind(Stmt, TagFields),
+	ok = esqlite3:fetchone(Stmt),
 	{noreply, State}.
 
-terminate(Reason, State) ->
+terminate(Reason, #{conn := Conn}) ->
 	ar:info([{ar_sqlite3, terminate}, {reason, Reason}]),
-	#{
-		insert_block_stmt := InsertBlockStmt,
-		insert_tx_stmt := InsertTxStmt,
-		insert_tag_stmt := InsertTagStmt,
-		select_tx_by_id_stmt := SelectTxByIdStmt,
-		select_block_by_tx_id_stmt := SelectBlockByTxIdStmt,
-		select_tags_by_tx_id_stmt := SelectTagsByTxIdStmt
-	} = State,
-	ok = sqlite3:finalize(?DB_NAME, SelectTagsByTxIdStmt),
-	ok = sqlite3:finalize(?DB_NAME, SelectBlockByTxIdStmt),
-	ok = sqlite3:finalize(?DB_NAME, SelectTxByIdStmt),
-	ok = sqlite3:finalize(?DB_NAME, InsertTagStmt),
-	ok = sqlite3:finalize(?DB_NAME, InsertTxStmt),
-	ok = sqlite3:finalize(?DB_NAME, InsertBlockStmt),
-	ok = sqlite3:close(?DB_NAME).
+	ok = esqlite3:close(Conn).
 
 %%%===================================================================
 %%% Internal functions.
 %%%===================================================================
 
-ensure_meta_table_created() ->
-	case sqlite3:sql_exec(?DB_NAME, "
+ensure_meta_table_created(Conn) ->
+	case esqlite3:q("
 		SELECT 1 FROM sqlite_master
 		WHERE type = 'table' AND name = 'migration'
-	") of
-		[{columns,["1"]},{rows,[{1}]}] -> ok;
-		[{columns,["1"]},{rows,[]}] -> create_meta_table()
+	", Conn) of
+		[{1}] -> ok;
+		[] -> create_meta_table(Conn)
 	end.
 
-create_meta_table() ->
+create_meta_table(Conn) ->
 	ar:info([{ar_sqlite3, creating_meta_table}]),
-	ok = sqlite3:sql_exec(?DB_NAME, ?CREATE_MIGRATION_TABLE_SQL),
+	ok = esqlite3:exec(?CREATE_MIGRATION_TABLE_SQL, Conn),
 	ok.
 
-ensure_schema_created() ->
-	case sqlite3:sql_exec(?DB_NAME, "
+ensure_schema_created(Conn) ->
+	case esqlite3:q("
 		SELECT 1 FROM migration
 		WHERE name = '20191009160000_schema_created'
-	") of
-		[{columns,["1"]},{rows,[{1}]}] -> ok;
-		[{columns,["1"]},{rows,[]}] -> create_schema()
+	", Conn) of
+		[{1}] -> ok;
+		[] -> create_schema(Conn)
 	end.
 
-create_schema() ->
+create_schema(Conn) ->
 	ar:info([{ar_sqlite3, creating_schema}]),
-	ok = sqlite3:sql_exec(?DB_NAME, "BEGIN TRANSACTION"),
-	[ok, ok, ok] = sqlite3:sql_exec_script_timeout(?DB_NAME, ?CREATE_TABLES_SQL, infinity),
-	[ok, ok, ok, ok, ok, ok] = sqlite3:sql_exec_script_timeout(?DB_NAME, ?CREATE_INDEXES_SQL, infinity),
-	{rowid, _} = sqlite3:sql_exec(?DB_NAME, "INSERT INTO migration VALUES ('20191009160000_schema_created', ?)", [sql_now()]),
-	ok = sqlite3:sql_exec(?DB_NAME, "COMMIT TRANSACTION"),
+	ok = esqlite3:exec("BEGIN TRANSACTION", Conn),
+	ok = esqlite3:exec(?CREATE_TABLES_SQL, Conn),
+	ok = esqlite3:exec(?CREATE_INDEXES_SQL, Conn),
+	'$done' = esqlite3:exec("INSERT INTO migration VALUES ('20191009160000_schema_created', ?)", [sql_now()], Conn),
+	ok = esqlite3:exec("COMMIT TRANSACTION", Conn),
 	ok.
 
-ensure_db_populated(BHL, State) ->
-	case sqlite3:sql_exec(?DB_NAME, "
+ensure_db_populated(BHL, #{ conn := Conn} = State) ->
+	case esqlite3:q("
 		SELECT 1 FROM migration
 		WHERE name = '20191015153000_db_populated'
-	") of
-		[{columns,["1"]},{rows,[{1}]}] ->
+	", Conn) of
+		[{1}] ->
 			ok;
-		[{columns,["1"]},{rows,[]}] ->
+		[] ->
 			ar:info([{ar_sqlite3, populating_db}]),
 			{Time, ok} = timer:tc(fun() -> ok = do_populate_db(BHL, State) end),
 			ar:info([{ar_sqlite3, populated_db}, {time, Time}]),
 			ok
 	end.
 
-do_populate_db(BHL, State) ->
-	ok = sqlite3:sql_exec(?DB_NAME, "BEGIN TRANSACTION"),
-	[ok, ok, ok, ok, ok, ok] = sqlite3:sql_exec_script(?DB_NAME, ?DROP_INDEXES_SQL),
+do_populate_db(BHL, #{ conn := Conn} = State) ->
+	ok = esqlite3:exec("BEGIN TRANSACTION", Conn),
+	ok = esqlite3:exec(?DROP_INDEXES_SQL, Conn),
 	ok = lists:foreach(fun(BH) ->
 		ok = insert_block_json(BH, State)
 	end, BHL),
-	[ok, ok, ok, ok, ok, ok] = sqlite3:sql_exec_script_timeout(?DB_NAME, ?CREATE_INDEXES_SQL, infinity),
-	{rowid, _} = sqlite3:sql_exec(?DB_NAME, "INSERT INTO migration VALUES ('20191015153000_db_populated', ?)", [sql_now()]),
-	ok = sqlite3:sql_exec(?DB_NAME, "COMMIT TRANSACTION"),
+	ok = esqlite3:exec(?CREATE_INDEXES_SQL, Conn, infinity),
+	'$done' = esqlite3:exec("INSERT INTO migration VALUES ('20191015153000_db_populated', ?)", [sql_now()], Conn),
+	ok = esqlite3:exec("COMMIT TRANSACTION", Conn, infinity),
 	ok.
 
 insert_block_json(BH,  Env) ->
 	#{ data_dir := DataDir, insert_block_stmt := Stmt } = Env,
 	case read_block(BH, DataDir) of
 		{ok, BlockMap} ->
-			ok = sqlite3:bind(?DB_NAME, Stmt, [
+			ok = esqlite3:bind(Stmt, [
 				maps:get(<<"indep_hash">>, BlockMap),
 				maps:get(<<"previous_block">>, BlockMap),
 				maps:get(<<"height">>, BlockMap),
 				maps:get(<<"timestamp">>, BlockMap)
 			]),
-			done = sqlite3:next(?DB_NAME, Stmt),
+			ok = esqlite3:fetchone(Stmt),
 			ok = lists:foreach(fun(TXHash) ->
 				ok = insert_tx_json(TXHash, maps:get(<<"indep_hash">>, BlockMap), Env)
 			end, maps:get(<<"txs">>, BlockMap)),
-			ok = sqlite3:reset(?DB_NAME, Stmt),
 			ok;
 		not_found ->
 			ok
@@ -338,7 +317,7 @@ insert_tx_json(TXHash, BlockIndepHash, Env) ->
 	#{ insert_tx_stmt := Stmt, data_dir := DataDir } = Env,
 	case read_tx(TXHash, DataDir) of
 		{ok, TxMap} ->
-			ok = sqlite3:bind(?DB_NAME, Stmt, [
+			ok = esqlite3:bind(Stmt, [
 				maps:get(<<"id">>, TxMap),
 				BlockIndepHash,
 				maps:get(<<"last_tx">>, TxMap),
@@ -349,24 +328,22 @@ insert_tx_json(TXHash, BlockIndepHash, Env) ->
 				maps:get(<<"signature">>, TxMap),
 				maps:get(<<"reward">>, TxMap)
 			]),
-			done = sqlite3:next(?DB_NAME, Stmt),
+			ok = esqlite3:fetchone(Stmt),
 			ok = lists:foreach(fun(TagMap) ->
 				ok = insert_tag_json(TagMap, maps:get(<<"id">>, TxMap), Env)
 			end, maps:get(<<"tags">>, TxMap)),
-			sqlite3:reset(?DB_NAME, Stmt),
 			ok;
 		not_found ->
 			ok
 	end.
 
 insert_tag_json(TagMap, TXID, #{ insert_tag_stmt := Stmt }) ->
-	ok = sqlite3:bind(?DB_NAME, Stmt, [
+	ok = esqlite3:bind(Stmt, [
 		TXID,
 		ar_util:decode(maps:get(<<"name">>, TagMap)),
 		ar_util:decode(maps:get(<<"value">>, TagMap))
 	]),
-	done = sqlite3:next(?DB_NAME, Stmt),
-	ok = sqlite3:reset(?DB_NAME, Stmt),
+	ok = esqlite3:fetchone(Stmt),
 	ok.
 
 read_block(BH, _DataDir) ->
@@ -462,17 +439,6 @@ select_txs_by_where_clause_tags(Tags) ->
 			}
 	end, {"1", []}, Tags).
 
-collect_txs(Stmt) ->
-	collect_txs(Stmt, []).
-
-collect_txs(Stmt, Acc) ->
-	case sqlite3:next(?DB_NAME, Stmt) of
-		Tuple when is_tuple(Tuple) ->
-			collect_txs(Stmt, [tx_map(Tuple) | Acc]);
-		done ->
-			lists:reverse(Acc)
-	end.
-
 tx_map({
 	Id,
 	BlockIndepHash,
@@ -506,17 +472,6 @@ block_map({
 	height => Height,
 	timestamp => Timestamp
 }.
-
-collect_tags(Stmt) ->
-	collect_tags(Stmt, []).
-
-collect_tags(Stmt, Acc) ->
-	case sqlite3:next(?DB_NAME, Stmt) of
-		Tuple when is_tuple(Tuple) ->
-			collect_tags(Stmt, [tags_map(Tuple) | Acc]);
-		done ->
-			lists:reverse(Acc)
-	end.
 
 tags_map({TxId, Name, Value}) ->
 	#{
@@ -571,17 +526,6 @@ eval_legacy_arql_where_clause({'or',E1,E2}) ->
 	};
 eval_legacy_arql_where_clause(_) ->
 	throw(bad_query).
-
-collect_txids(Stmt) ->
-	collect_txids(Stmt, []).
-
-collect_txids(Stmt, Acc) ->
-	case sqlite3:next(?DB_NAME, Stmt) of
-		{EncodedTXID} ->
-			collect_txids(Stmt, [EncodedTXID | Acc]);
-		done ->
-			lists:reverse(Acc)
-	end.
 
 full_block_to_fields(FullBlock) ->
 	BlockIndepHash = ar_util:encode(FullBlock#block.indep_hash),
