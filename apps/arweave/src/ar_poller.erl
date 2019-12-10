ArweaveTeam/arweave

commit 4d9a401de26051930a503d3433d34fc8e2c1e51f

Reconstruct hash list in polling mode efficiently
In the polling mode, in case the block shadow does not match
the current hash, download preceding shadows until the hashes match.

Co-authored-by: Maximilien Mellen <max@arweave.org>

apps/arweave/src/ar_poller.erl
@@ -90,19 +90,16 @@ poll_block_step(check_ignore_list, {Peer, BShadow}) ->
 			{error, block_already_received};
 		false ->
 			ar_bridge:ignore_id(BShadow#block.indep_hash),
-			poll_block_step(check_hash_list, {Peer, BShadow})
+			poll_block_step(construct_hash_list, {Peer, BShadow})
 	end;
-poll_block_step(check_hash_list, {Peer, BShadow}) ->
+poll_block_step(construct_hash_list, {Peer, BShadow}) ->
 	{ok, BlockTXsPairs} = ar_node:get_block_txs_pairs(whereis(http_entrypoint_node)),
 	HL = lists:map(fun({BH, _}) -> BH end, BlockTXsPairs),
-	PrevH = BShadow#block.previous_block,
-	case HL of
-		[PrevH | _] ->
-			poll_block_step(accept_block, {Peer, BShadow#block{ hash_list = HL }});
-		_ ->
-			PeerHL = ar_http_iface_client:get_hash_list(Peer),
-			BlockHL = reconstruct_block_hash_list(BShadow#block.indep_hash, PeerHL),
-			poll_block_step(accept_block, {Peer, BShadow#block{ hash_list = BlockHL }})
+	case reconstruct_block_hash_list(Peer, BShadow, HL) of
+		{ok, BHL} ->
+			poll_block_step(accept_block, {Peer, BShadow#block{ hash_list = BHL }});
+		{error, _} = Error ->
+			Error
 	end;
 poll_block_step(accept_block, {Peer, BShadow}) ->
 	Node = whereis(http_entrypoint_node),
@@ -110,9 +107,21 @@ poll_block_step(accept_block, {Peer, BShadow}) ->
 	Node ! {new_block, Peer, BShadowHeight, BShadow, no_data_segment, no_recall},
 	ok.
 
-reconstruct_block_hash_list(_H, []) ->
-	{error, failed_to_reconstruct_block_hash_list};
-reconstruct_block_hash_list(H, [H | Rest]) ->
-	lists:sublist(Rest, ?STORE_BLOCKS_BEHIND_CURRENT);
-reconstruct_block_hash_list(H, [_ | HL]) ->
-	reconstruct_block_hash_list(H, HL).
+reconstruct_block_hash_list(Peer, BShadow, HL) ->
+	reconstruct_block_hash_list(Peer, BShadow, HL, []).
+
+reconstruct_block_hash_list(Peer, BShadow, HL, BHL) ->
+	PrevH = BShadow#block.previous_block,
+	case HL of
+		[PrevH | _] = L ->
+			{ok, lists:reverse(BHL) ++ L};
+		[_ | HLTail] ->
+			case ar_http_iface_client:get_block_shadow([Peer], PrevH) of
+				unavailable ->
+					{error, previous_block_not_found};
+				{_, PrevBShadow} ->
+					reconstruct_block_hash_list(Peer, PrevBShadow, HLTail, [PrevH | BHL])
+			end;
+		[] ->
+			{error, failed_to_reconstruct_block_hash_list}
+	end.

commit 90bc1c7cdcc8edef3e816622ee2e9fed930685fc

Fix incorrect fork handling in the polling mode
Regression was introduced in 4d9a401de26051930a503d3433d34fc8e2c1e51f.

Co-authored-by: Maximilien Mellen <max@arweave.org>

apps/arweave/src/ar_poller.erl
@@ -109,7 +109,8 @@ poll_block_step(check_ignore_list, {Peer, BShadow}) ->
 			end
 	end;
 poll_block_step(construct_hash_list, {Peer, BShadow}) ->
-	{ok, BlockTXsPairs} = ar_node:get_block_txs_pairs(whereis(http_entrypoint_node)),
+	Node = whereis(http_entrypoint_node),
+	{ok, BlockTXsPairs} = ar_node:get_block_txs_pairs(Node),
 	HL = lists:map(fun({BH, _}) -> BH end, BlockTXsPairs),
 	case reconstruct_block_hash_list(Peer, BShadow, HL) of
 		{ok, BHL} ->
@@ -123,21 +124,22 @@ poll_block_step(accept_block, {Peer, BShadow}) ->
 	Node ! {new_block, Peer, BShadowHeight, BShadow, no_data_segment, no_recall},
 	ok.
 
-reconstruct_block_hash_list(Peer, BShadow, HL) ->
-	reconstruct_block_hash_list(Peer, BShadow, HL, []).
+reconstruct_block_hash_list(Peer, FetchedBShadow, BehindCurrentHL) ->
+	reconstruct_block_hash_list(Peer, FetchedBShadow, BehindCurrentHL, []).
 
-reconstruct_block_hash_list(Peer, BShadow, HL, BHL) ->
-	PrevH = BShadow#block.previous_block,
-	case HL of
+reconstruct_block_hash_list(_Peer, _FetchedBShadow, _BehindCurrentHL, FetchedHL)
+		when length(FetchedHL) >= ?STORE_BLOCKS_BEHIND_CURRENT ->
+	{error, failed_to_reconstruct_block_hash_list};
+reconstruct_block_hash_list(Peer, FetchedBShadow, BehindCurrentHL, FetchedHL) ->
+	PrevH = FetchedBShadow#block.previous_block,
+	case lists:dropwhile(fun(H) -> H /= PrevH end, BehindCurrentHL) of
 		[PrevH | _] = L ->
-			{ok, lists:reverse(BHL) ++ L};
-		[_ | HLTail] ->
+			{ok, lists:sublist(lists:reverse(FetchedHL) ++ L, ?STORE_BLOCKS_BEHIND_CURRENT)};
+		_ ->
 			case ar_http_iface_client:get_block_shadow([Peer], PrevH) of
 				unavailable ->
 					{error, previous_block_not_found};
 				{_, PrevBShadow} ->
-					reconstruct_block_hash_list(Peer, PrevBShadow, HLTail, [PrevH | BHL])
-			end;
-		[] ->
-			{error, failed_to_reconstruct_block_hash_list}
+					reconstruct_block_hash_list(Peer, PrevBShadow, BehindCurrentHL, [PrevH | FetchedHL])
+			end
 	end.