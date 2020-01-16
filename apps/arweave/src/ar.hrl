4f5d822d28b8848f224483a771de15e365952d0b
Lev Berman <ldmberman@protonmail.com>
2020-01-16 15:44:51 +0100

    FIXUP remove unused constants

--- apps/arweave/src/ar.hrl
+++ apps/arweave/src/ar.hrl
@@ -266,12 +266,7 @@
 %% 512KB should be enough -- future, please don't hate me.
 -define(DATA_CHUNK_SIZE, 512 * 1024).
 -define(MAX_PATH_SIZE, 256 * 1024).
-%% @doc The maximum size of the chunk hash list in bytes. Shorter chunk IDs
-%% (configurable by the user on a per-TX basis) give lower safety in terms of
-%% the potential for hash collisions, but higher transaction size limits.
--define(MAX_CHUNK_LIST_SIZE, 384 * 1024).
 -define(CHUNK_ID_HASH_SIZE, 32).
--define(DEFAULT_CHUNK_ALG, "sha2-256").
 
 %% @doc A full block or block shadow (see more on txs field).
 -record(block, {
