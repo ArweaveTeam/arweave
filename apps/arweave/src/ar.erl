bf397d7a42450a26e8f5ef1be0d5050ce6a6543f
Lev Berman <ldmberman@protonmail.com>
2020-01-30 16:53:01 +0100

    fixup! Mock RandomX in tests

--- apps/arweave/src/ar.erl
+++ apps/arweave/src/ar.erl
@@ -56,7 +56,6 @@
 		ar_multiple_txs_per_wallet_tests,
 		ar_tx_perpetual_storage_tests,
 		ar_gateway_middleware_tests,
-		ar_randomx_mining_tests,
 		ar_http_util_tests,
 		% ar_meta_db must be the last in the list since it resets global configuration
 		ar_meta_db
