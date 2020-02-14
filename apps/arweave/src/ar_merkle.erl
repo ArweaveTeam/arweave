3c8908b71fa17487d21dbf5e3311ed232e354b39
Lev Berman <ldmberman@protonmail.com>
2020-02-14 13:18:45 +0100

    FIXUP fix indentation in ar_merkle.erl

--- apps/arweave/src/ar_merkle.erl
+++ apps/arweave/src/ar_merkle.erl
@@ -10,13 +10,13 @@
 %%% as verification of those proofs.
 
 -record(node, {
-    id,
-    type = branch, % root | branch | leaf
-    data, % The value (for leaves)
-    note, % A number less than 2^64
-    left, % The (optional) ID of a node to the left
-    right, % The (optional) ID of a node to the right
-    max % The maximum observed note at this point
+	id,
+	type = branch, % root | branch | leaf
+	data, % The value (for leaves)
+	note, % A number less than 2^64
+	left, % The (optional) ID of a node to the left
+	right, % The (optional) ID of a node to the right
+	max % The maximum observed note at this point
 }).
 
 -define(HASH_SIZE, ?CHUNK_ID_HASH_SIZE).
@@ -28,7 +28,7 @@
 %% @doc Generate a tree from a list of pairs of IDs (of length 32 bytes)
 %% and labels -- most often sizes.
 generate_tree(Elements) ->
-    generate_all_rows(generate_leaves(Elements)).
+	generate_all_rows(generate_leaves(Elements)).
 
 generate_leaves(Elements) ->
 	lists:foldr(
@@ -53,121 +53,121 @@ generate_leaves(Elements) ->
 %% The produced trees could be a little smaller if these duplicates were 
 %% not present, but removing them with `ar_util:unique` takes far too long.
 generate_all_rows([]) ->
-    {<<>>, []};
+	{<<>>, []};
 generate_all_rows(Leaves) ->
-    generate_all_rows(Leaves, Leaves).
+	generate_all_rows(Leaves, Leaves).
 
 generate_all_rows([RootN], Tree) ->
-    {RootN#node.id, Tree};
+	{RootN#node.id, Tree};
 generate_all_rows(Row, Tree) ->
-    NewRow = generate_row(Row),
-    generate_all_rows(NewRow, NewRow ++ Tree).
+	NewRow = generate_row(Row),
+	generate_all_rows(NewRow, NewRow ++ Tree).
 
 generate_row([]) -> [];
 generate_row([Left]) -> [generate_node(Left, empty)];
 generate_row([L, R | Rest]) ->
-    [generate_node(L, R) | generate_row(Rest)].
+	[generate_node(L, R) | generate_row(Rest)].
 
 generate_node(Left, empty) ->
-    Left;
+	Left;
 generate_node(L, R) ->
-    #node {
-        id = hash([hash(L#node.id), hash(R#node.id), hash(note_to_binary(L#node.max))]),
-        type = branch,
-        left = L#node.id,
-        right = R#node.id,
-        note = L#node.max,
-        max = R#node.max
-    }.
+	#node {
+		id = hash([hash(L#node.id), hash(R#node.id), hash(note_to_binary(L#node.max))]),
+		type = branch,
+		left = L#node.id,
+		right = R#node.id,
+		note = L#node.max,
+		max = R#node.max
+	}.
 
 %%% Merkle path generation and verification functions.
 
 generate_path(ID, Dest, Tree) ->
-    binary:list_to_bin(generate_path_parts(ID, Dest, Tree)).
+	binary:list_to_bin(generate_path_parts(ID, Dest, Tree)).
 
 generate_path_parts(ID, Dest, Tree) ->
-    case get(ID, Tree) of
-        N when N#node.type == leaf ->
-            [N#node.data, note_to_binary(N#node.note)];
-        N when N#node.type == branch ->
-            [
-                N#node.left, N#node.right, note_to_binary(N#node.note)
-            |
-                generate_path_parts(
-                    case Dest < N#node.note of
-                        true -> N#node.left;
-                        false -> N#node.right
-                    end,
-                    Dest,
-                    Tree
-                )
-            ]
-    end.
+	case get(ID, Tree) of
+		N when N#node.type == leaf ->
+			[N#node.data, note_to_binary(N#node.note)];
+		N when N#node.type == branch ->
+			[
+				N#node.left, N#node.right, note_to_binary(N#node.note)
+			|
+				generate_path_parts(
+					case Dest < N#node.note of
+						true -> N#node.left;
+						false -> N#node.right
+					end,
+					Dest,
+					Tree
+				)
+			]
+	end.
 
 validate_path(ID, _Dest, << Data:?HASH_SIZE/binary, Note:(?NOTE_SIZE*8) >>) ->
-    case hash([hash(Data), hash(note_to_binary(Note))]) of
-        ID -> Data;
-        _ -> false
-    end;
+	case hash([hash(Data), hash(note_to_binary(Note))]) of
+		ID -> Data;
+		_ -> false
+	end;
 validate_path(ID, Dest,
-        << L:?HASH_SIZE/binary, R:?HASH_SIZE/binary, Note:(?NOTE_SIZE*8), Rest/binary >>) ->
-    case hash([hash(L), hash(R), hash(note_to_binary(Note))]) of
-        ID ->
-            validate_path(
-                case Dest < Note of
-                    true -> L;
-                    false -> R
-                end,
-                Dest,
-                Rest
-            );
-        _ -> false
-    end.
+		<< L:?HASH_SIZE/binary, R:?HASH_SIZE/binary, Note:(?NOTE_SIZE*8), Rest/binary >>) ->
+	case hash([hash(L), hash(R), hash(note_to_binary(Note))]) of
+		ID ->
+			validate_path(
+				case Dest < Note of
+					true -> L;
+					false -> R
+				end,
+				Dest,
+				Rest
+			);
+		_ -> false
+	end.
 
 %% @doc Get the note attached to the final node from a path.
 extract_note(Path) ->
-    binary:decode_unsigned(
-        binary:part(Path, byte_size(Path) - ?NOTE_SIZE, ?NOTE_SIZE)
-    ).
+	binary:decode_unsigned(
+		binary:part(Path, byte_size(Path) - ?NOTE_SIZE, ?NOTE_SIZE)
+	).
 
 %%% Helper functions for managing the tree data structure.
 %%% Abstracted so that the concrete data type can be replaced later.
 
 new() ->
-    [].
+	[].
 
 insert(Node, Map) ->
-    [Node | Map].
+	[Node | Map].
 
 get(ID, Map) ->
-    case lists:keyfind(ID, #node.id, Map) of
-        false -> false;
-        Node -> Node
-    end.
+	case lists:keyfind(ID, #node.id, Map) of
+		false -> false;
+		Node -> Node
+	end.
 
 note_to_binary(Note) ->
-    << Note:(?NOTE_SIZE * 8) >>.
+	<< Note:(?NOTE_SIZE * 8) >>.
 
 hash(Parts) when is_list(Parts) ->
-    crypto:hash(sha256, binary:list_to_bin(Parts));
+	crypto:hash(sha256, binary:list_to_bin(Parts));
 hash(Binary) ->
 	crypto:hash(sha256, Binary).
 
 %%% Helpers
 
 make_tags_cumulative(L) ->
-    lists:reverse(
-        element(2,
-            lists:foldl(
-                fun({X, Tag}, {AccTag, AccL}) ->
-                    Curr = AccTag + Tag,
-                    {Curr, [{X, Curr} | AccL]}
-                end,
-                {0, []},
-                L
-            )
-        )
-    ).
+	lists:reverse(
+		element(2,
+			lists:foldl(
+				fun({X, Tag}, {AccTag, AccL}) ->
+					Curr = AccTag + Tag,
+					{Curr, [{X, Curr} | AccL]}
+				end,
+				{0, []},
+				L
+			)
+		)
+	).
 
 %%% Tests
 
@@ -176,46 +176,46 @@ make_tags_cumulative(L) ->
 -define(UNEVEN_TEST_TARGET, 33271).
 
 generate_balanced_tree_test() ->
-    {_MR, Tree} =
+	{_MR, Tree} =
 		ar_merkle:generate_tree(make_tags_cumulative([{<<N:256>>, 1} || N <- lists:seq(0, ?TEST_SIZE - 1)])),
-    ?assertEqual(length(Tree), (?TEST_SIZE * 2) - 1).
+	?assertEqual(length(Tree), (?TEST_SIZE * 2) - 1).
 
 generate_and_validate_balanced_tree_path_test() ->
-    {MR, Tree} =
+	{MR, Tree} =
 		ar_merkle:generate_tree(make_tags_cumulative([{<<N:256>>, 1} || N <- lists:seq(0, ?TEST_SIZE - 1)])),
-    RandomTarget = rand:uniform(?TEST_SIZE) - 1,
-    ?assertEqual(
-        RandomTarget,
-        binary:decode_unsigned(
-            ar_merkle:validate_path(
-                MR, RandomTarget,
-                ar_merkle:generate_path(MR, RandomTarget, Tree)
-            )
-        )
-    ).
+	RandomTarget = rand:uniform(?TEST_SIZE) - 1,
+	?assertEqual(
+		RandomTarget,
+		binary:decode_unsigned(
+			ar_merkle:validate_path(
+				MR, RandomTarget,
+				ar_merkle:generate_path(MR, RandomTarget, Tree)
+			)
+		)
+	).
 
 generate_and_validate_uneven_tree_path_test() ->
-    {MR, Tree} =
+	{MR, Tree} =
 		ar_merkle:generate_tree(make_tags_cumulative([{<<N:256>>, 1} || N <- lists:seq(0, ?UNEVEN_TEST_SIZE - 1)])),
-    %% Make sure the target is in the 'uneven' ending of the tree.
-    ?assertEqual(
-        ?UNEVEN_TEST_TARGET,
-        binary:decode_unsigned(
-            ar_merkle:validate_path(
-                MR, ?UNEVEN_TEST_TARGET,
-                ar_merkle:generate_path(MR, ?UNEVEN_TEST_TARGET, Tree)
-            )
-        )
-    ).
+	%% Make sure the target is in the 'uneven' ending of the tree.
+	?assertEqual(
+		?UNEVEN_TEST_TARGET,
+		binary:decode_unsigned(
+			ar_merkle:validate_path(
+				MR, ?UNEVEN_TEST_TARGET,
+				ar_merkle:generate_path(MR, ?UNEVEN_TEST_TARGET, Tree)
+			)
+		)
+	).
 
 reject_invalid_tree_path_test() ->
-    {MR, Tree} =
+	{MR, Tree} =
 		ar_merkle:generate_tree(make_tags_cumulative([{<<N:256>>, 1} || N <- lists:seq(0, ?TEST_SIZE - 1)])),
-    RandomTarget = rand:uniform(?TEST_SIZE) - 1,
-    ?assertEqual(
-        false,
-        ar_merkle:validate_path(
-            MR, RandomTarget,
-            ar_merkle:generate_path(MR, 1000, Tree)
-        )
-    ).
+	RandomTarget = rand:uniform(?TEST_SIZE) - 1,
+	?assertEqual(
+		false,
+		ar_merkle:validate_path(
+			MR, RandomTarget,
+			ar_merkle:generate_path(MR, 1000, Tree)
+		)
+	).
