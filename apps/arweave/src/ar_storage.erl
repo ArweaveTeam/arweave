bc57b5c31510f690d63aae665f3add2571b9dd83
Lev Berman <ldmberman@protonmail.com>
2020-02-28 12:20:41 +0100

    Do not do wildcard query in read_tx

--- apps/arweave/src/ar_storage.erl
+++ apps/arweave/src/ar_storage.erl
@@ -307,20 +307,12 @@ read_tx(TX) when is_record(TX, tx) -> TX;
 read_tx(TXs) when is_list(TXs) ->
 	lists:map(fun read_tx/1, TXs);
 read_tx(ID) ->
-	case filelib:wildcard(tx_filepath(ID)) of
-		[] -> unavailable;
-		[Filename] -> read_tx_file(Filename);
-		Filenames ->
-			read_tx_file(hd(
-				lists:sort(
-					fun(Filename, Filename2) ->
-						{ok, Info} = file:read_file_info(Filename, [{time, posix}]),
-						{ok, Info2} = file:read_file_info(Filename2, [{time, posix}]),
-						Info#file_info.mtime >= Info2#file_info.mtime
-					end,
-					Filenames
-				)
-			))
+	Filename = tx_filepath(ID),
+	case filelib:is_file(Filename) of
+		false ->
+			unavailable;
+		true ->
+			read_tx_file(Filename)
 	end.
 
 read_tx_file(Filename) ->
