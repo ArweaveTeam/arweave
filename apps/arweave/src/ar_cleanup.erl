3bdb6dad3e0e2e00d6144941b75943521a7a3174
Lev Berman <ldmberman@protonmail.com>
2020-04-10 18:23:54 +0200

    Add a script for removing old wallet lists

--- apps/arweave/src/ar_cleanup.erl
+++ apps/arweave/src/ar_cleanup.erl
@@ -1,9 +1,11 @@
 -module(ar_cleanup).
 
--export([rewrite/0, rewrite/1]).
+-export([rewrite/0, rewrite/1, remove_old_wallet_lists/0]).
 
 -include("ar.hrl").
 -include_lib("eunit/include/eunit.hrl").
+
+-define(KEEP_WALLET_LISTS, 1000).
 
 %% @doc Rewrite every block in the hash list using the latest format.
 %% In the case of upgrading a node from 1.1 to 1.5, this dramatically reduces
@@ -23,3 +25,49 @@ rewrite([{H, _, _} | Rest], BI) ->
 		ar:report([{error_rewriting_block, ar_util:encode(H)}])
 	end,
 	rewrite(Rest, BI).
+
+remove_old_wallet_lists() ->
+	DataDir = rpc:call('arweave@127.0.0.1', ar_meta_db, get, [data_dir], 5000),
+	WalletListDir = filename:join(DataDir, ?WALLET_LIST_DIR),
+	case file:list_dir(WalletListDir) of
+		{ok, Filenames} ->
+			WalletListFilepaths = lists:filtermap(
+				fun(Filename) ->
+					case string:split(Filename, ".json") of
+						[Hash, []] when length(Hash) > 0 ->
+							{true, filename:join(WalletListDir, Filename)};
+						_ ->
+							false
+					end
+				end,
+				Filenames
+			),
+			remove_old_wallet_lists(WalletListFilepaths);
+		{error, Reason} ->
+			io:format("~nFailed to scan the disk, reason ~p", [Reason]),
+			erlang:halt(1)
+	end.
+
+remove_old_wallet_lists(Filepaths) when length(Filepaths) =< ?KEEP_WALLET_LISTS ->
+	io:format("~nCurrently less than ~B wallets on disk, nothing to clean.~n~n", [?KEEP_WALLET_LISTS]),
+	erlang:halt(0);
+remove_old_wallet_lists(Filepaths) ->
+	SortedFilepaths =
+		lists:sort(
+			fun(A, B) -> filelib:last_modified(A) < filelib:last_modified(B) end,
+			Filepaths
+		),
+	ToRemove = lists:sublist(SortedFilepaths, length(Filepaths) - ?KEEP_WALLET_LISTS),
+	lists:foreach(
+		fun(File) ->
+			case file:delete(File) of
+				ok ->
+					ok;
+				{error, Reason} ->
+					io:format("~nFailed to remove file ~s for reason ~p.~n~n", [File, Reason])
+			end
+		end,
+		ToRemove
+	),
+	io:format("~nCleanup complete.~n~n"),
+	erlang:halt(0).
