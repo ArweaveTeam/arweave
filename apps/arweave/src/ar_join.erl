989167ef9e5e4ae7581428d5f40da8e64ed65004
Lev Berman <ldmberman@protonmail.com>
2020-09-29 15:50:27 +0200

    Only communicate to peers from the same network
    The main motivation is to be able to run a custom weave (tests, local development, benchmarking, testnet)
    on the same IP which used to belong to mainnet and without the risk of having the address banned
    by mainnet peers for an invalid block, lose peer reputation, or simply pollute the traffic.

--- apps/arweave/src/ar_join.erl
+++ apps/arweave/src/ar_join.erl
@@ -2,7 +2,8 @@
 
 -export([
 	start/2,
-	start/3
+	start/3,
+	filter_peer_list/1
 ]).
 
 -include("ar.hrl").
@@ -36,45 +37,25 @@ start(Node, RawPeers, {NewB, BI}) ->
 
 %% @doc Perform the joining process.
 do_join(_Node, _RawPeers, NewB, _BI) when not ?IS_BLOCK(NewB) ->
-	ar:report_console(
-		[
-			node_not_joining,
-			{reason, cannot_get_full_block_from_peer},
-			{received_instead, NewB}
-		]
-	);
-do_join(Node, RawPeers, NewB, BI) ->
-	register(join_server, self()),
-	case verify_time_sync(RawPeers) of
-		false ->
-			ar:err(
-				[
-					node_not_joining,
-					{reason, clock_time_in_sync_with_join_peers},
-					{
-						recommendation,
-						"Ensure that your machine's time is up-to-date."
-					}
-				]
-			);
-		true ->
-			Peers = filter_peer_list(RawPeers),
-			ar:report_console(
-				[
-					joining_network,
-					{node, Node},
-					{peers, Peers},
-					{height, NewB#block.height}
-				]
-			),
-			ar_miner_log:joining(),
-			ar_arql_db:populate_db(?BI_TO_BHL(BI)),
-			ar_randomx_state:init(BI, Peers),
-			Blocks = get_block_and_trail(Peers, NewB, BI),
-			Node ! {join, BI, Blocks},
-			join_peers(Peers),
-			ar_miner_log:joined()
-	end.
+	ar:report_console([
+		{event, node_not_joining},
+		{reason, cannot_get_full_block_from_peer},
+		{received_instead, NewB}
+	]);
+do_join(Node, Peers, NewB, BI) ->
+	ar:report_console([
+		{event, joining_network},
+		{node, Node},
+		{peers, [ar_util:format_peer(Peer) || Peer <- Peers]},
+		{height, NewB#block.height}
+	]),
+	ar_miner_log:joining(),
+	ar_arql_db:populate_db(?BI_TO_BHL(BI)),
+	ar_randomx_state:init(BI, Peers),
+	Blocks = get_block_and_trail(Peers, NewB, BI),
+	Node ! {join, BI, Blocks},
+	join_peers(Peers),
+	ar_miner_log:joined().
 
 %% @doc Verify timestamps of peers.
 verify_time_sync(Peers) ->
@@ -158,36 +139,29 @@ find_current_block([Peer | Tail]) ->
 			find_current_block(Tail)
 	end.
 
-%% @doc Verify peer(s) are on the same network as the client. Remove any that
-%% are not.
--ifdef(DEBUG).
-filter_peer_list(Peers) when is_list(Peers) ->
-	lists:filter(
-		fun(Peer) when is_pid(Peer) -> true;
-		   (Peer) -> ar_http_iface_client:get_info(Peer, name) == <<?NETWORK_NAME>>
-		end,
-	Peers);
-filter_peer_list(Peer) -> filter_peer_list([Peer]).
--else.
-filter_peer_list(Peers) when is_list(Peers) ->
-	lists:filter(
-		fun(Peer) when is_pid(Peer) -> false;
-		   (Peer) -> ar_http_iface_client:get_info(Peer, name) == <<?NETWORK_NAME>>
-		end,
-	Peers);
-filter_peer_list(Peer) -> filter_peer_list([Peer]).
--endif.
+%% @doc Verify peers are on the same network as us and the clocks are in sync.
+filter_peer_list(Peers) ->
+	SameNetworkPeers =
+		lists:filter(
+			fun(Peer) ->
+				ar_http_iface_client:get_info(Peer, name) == <<?NETWORK_NAME>>
+			end,
+			Peers
+		),
+	case verify_time_sync(SameNetworkPeers) of
+		false ->
+			[];
+		true ->
+			SameNetworkPeers
+	end.
 
-
-join_peers(Peers) when is_list(Peers) ->
+join_peers(Peers) ->
 	lists:foreach(
-		fun(P) ->
-			join_peers(P)
+		fun(Peer) ->
+			ar_http_iface_client:add_peer(Peer)
 		end,
 		Peers
-	);
-join_peers(Peer) when is_pid(Peer) -> ok;
-join_peers(Peer) -> ar_http_iface_client:add_peer(Peer).
+	).
 
 %% @doc Get a block, and its 2 * ?MAX_TX_ANCHOR_DEPTH previous blocks.
 %% If the block list is shorter than 2 * ?MAX_TX_ANCHOR_DEPTH, simply
