82ba60fa7b11f933de60a576c94371cc6255ff29
Lev Berman <ldmberman@gmail.com>
2020-10-30 21:24:53 +0100

    Fix requests_per_minute_limit flag broken in fbc381e0

--- apps/arweave/src/ar_blacklist_middleware.hrl
+++ apps/arweave/src/ar_blacklist_middleware.hrl
@@ -2,20 +2,31 @@
 
 -define(BAN_CLEANUP_INTERVAL, 60000).
 
--ifdef(DEBUG).
 -define(RPM_BY_PATH(Path), fun() ->
+	DefaultPathLimit =
+		case ar_meta_db:get(requests_per_minute_limit) of
+			not_found ->
+				900;
+			Limit ->
+				Limit
+		end,
+	?RPM_BY_PATH(Path, DefaultPathLimit)()
+end).
+
+-ifdef(DEBUG).
+-define(RPM_BY_PATH(Path, DefaultPathLimit), fun() ->
 	case Path of
 		[<<"chunk">> | _]            -> {chunk,            6000};
 		[<<"data_sync_record">> | _] -> {data_sync_record, 50};
-		_ ->                            {default,          900}
+		_ ->                            {default,          DefaultPathLimit}
 	end
 end).
 -else.
--define(RPM_BY_PATH(Path), fun() ->
+-define(RPM_BY_PATH(Path, DefaultPathLimit), fun() ->
 	case Path of
 		[<<"chunk">> | _]            -> {chunk,            12000}; % sufficient to upload 50 MB/s
 		[<<"data_sync_record">> | _] -> {data_sync_record, 20};
-		_ ->                            {default,          900}
+		_ ->                            {default,          DefaultPathLimit}
 	end
 end).
 -endif.
