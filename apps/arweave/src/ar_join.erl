ArweaveTeam/arweave

commit f8f4771b465d09e92a59096344aba31b2ab7cff6

Refactor node startup, ar_bridge, state management
The main motivation is to improve maintainability of the code. Below is a detailed list of changes.

- Public ETS tables are initialized in ar_sup, their lifecycle is not bound to that of the corresponding processes;
- all supervisors are launched in ar_sup; there it is very clear which processes are run and what is the dependency graph;
- process-specific initialization takes place in the corresponding init functions making ar:start clean;
- accounts (wallets) are initialized asynchronously;
- trusted peers are fetched directly from the application config instead of ar_meta_db;
- ar_bridge is a gen_server;
- ar_bridge does not depend on ar_node on startup anymore - ar_node_worker now loads and processes persisted mempool;
- ar_node does not manage the state copy any longer - state is stored in ETS; it simplifies the startup and saves us from the need to synchronize the state across two processes;
- OTP logger is used instead of the outdated error_logger;
- log format is changed; logs are one-liners now;
- logs are rotated;
- the polling configuration parameter is an integer now.

The change comes with the tests for webhooks, ar_poller, and persisted mempool.

Co-authored-by: Lev Berman <ldmberman@protonmail.com>

apps/arweave/src/ar_join.erl
@@ -2,11 +2,10 @@
 
 -export([
 	start/2,
-	start/3,
-	filter_peer_list/1
+	start/3
 ]).
 
--include("ar.hrl").
+-include_lib("arweave/include/ar.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 %%% Represents a process that handles creating an initial, minimal
@@ -23,12 +22,12 @@ start(Node, Peers) ->
 	spawn(fun() -> start(Node, Peers, find_current_block(Peers)) end).
 
 start(_, [], _) ->
-	ar:report_console([not_joining, {reason, no_peers}]);
+	?LOG_INFO([{event, not_joining}, {reason, no_peers}]);
 start(Node, Peers, B) when is_atom(B) ->
-	ar:report_console(
+	?LOG_INFO(
 		[
-			could_not_retrieve_current_block,
-			{trying_again_in, ?REJOIN_TIMEOUT, seconds}
+			{event, could_not_retrieve_current_block},
+			{trying_again_in, ?REJOIN_TIMEOUT}
 		]
 	),
 	timer:apply_after(?REJOIN_TIMEOUT, ar_join, start, [Node, Peers]);
@@ -37,74 +36,26 @@ start(Node, RawPeers, {NewB, BI}) ->
 
 %% @doc Perform the joining process.
 do_join(_Node, _RawPeers, NewB, _BI) when not ?IS_BLOCK(NewB) ->
-	ar:report_console([
+	?LOG_INFO([
 		{event, node_not_joining},
 		{reason, cannot_get_full_block_from_peer},
 		{received_instead, NewB}
 	]);
-do_join(Node, Peers, NewB, BI) ->
-	ar:report_console([
-		{event, joining_network},
-		{node, Node},
-		{peers, [ar_util:format_peer(Peer) || Peer <- Peers]},
-		{height, NewB#block.height}
-	]),
-	ar_miner_log:joining(),
+do_join(_Node, Peers, NewB, BI) ->
+	ar:console("Joining the Arweave network...~n"),
 	ar_arql_db:populate_db(?BI_TO_BHL(BI)),
 	ar_randomx_state:init(BI, Peers),
 	Blocks = get_block_and_trail(Peers, NewB, BI),
-	Node ! {join, BI, Blocks},
+	ar_node_worker ! {join, BI, Blocks},
 	join_peers(Peers),
-	ar_miner_log:joined().
-
-%% @doc Verify timestamps of peers.
-verify_time_sync(Peers) ->
-	%% Ignore this check if time syncing is disabled.
-	case ar_meta_db:get(time_syncing) of
-		false -> true;
-		_ ->
-			VerifyPeerClock = fun(Peer) ->
-				case ar_http_iface_client:get_time(Peer, 5 * 1000) of
-					{ok, {RemoteTMin, RemoteTMax}} ->
-						LocalT = os:system_time(second),
-						Tolerance = ?JOIN_CLOCK_TOLERANCE,
-						case LocalT of
-							T when T < RemoteTMin - Tolerance ->
-								log_peer_clock_diff(Peer, RemoteTMin - Tolerance - T),
-								false;
-							T when T < RemoteTMin - Tolerance div 2 ->
-								log_peer_clock_diff(Peer, RemoteTMin - T),
-								true;
-							T when T > RemoteTMax + Tolerance ->
-								log_peer_clock_diff(Peer, T - RemoteTMax - Tolerance),
-								false;
-							T when T > RemoteTMax + Tolerance div 2 ->
-								log_peer_clock_diff(Peer, T - RemoteTMax),
-								true;
-							_ ->
-								true
-						end;
-					{error, Err} ->
-						ar:info(
-							"Failed to get time from peer ~s: ~p.",
-							[ar_util:format_peer(Peer), Err]
-						),
-						true
-				end
-			end,
-			Responses = ar_util:pmap(VerifyPeerClock, [P || P <- Peers, not is_pid(P)]),
-			lists:all(fun(R) -> R end, Responses)
-	end.
-
-log_peer_clock_diff(Peer, Diff) ->
-	Warning = "Your local clock deviates from peer ~s by ~B seconds or more.",
-	WarningArgs = [ar_util:format_peer(Peer), Diff],
-	ar:console(Warning, WarningArgs),
-	ar:warn(Warning, WarningArgs).
+	ar:console("Joined the Arweave network successfully.~n"),
+	?LOG_INFO([{event, joined_the_network}]).
 
 %% @doc Return the current block from a list of peers.
 find_current_block([]) ->
-	ar:info("Did not manage to fetch current block from any of the peers. Will retry later."),
+	ar:console(
+		"Did not manage to fetch current block from any of the peers. Will retry later.~n"
+	),
 	unavailable;
 find_current_block([Peer | Tail]) ->
 	try ar_http_iface_client:get_block_index(Peer) of
@@ -112,47 +63,29 @@ find_current_block([Peer | Tail]) ->
 			find_current_block(Tail);
 		BI ->
 			{Hash, _, _} = hd(BI),
-			ar:info([
-				"Fetching current block.",
-				{peer, Peer},
-				{hash, Hash}
-			]),
+			ar:console(
+				"Fetching current block. ~p ~p~n",
+				[{peer, ar_util:format_peer(Peer)}, {hash, ar_util:encode(Hash)}]
+			),
 			MaybeB = ar_http_iface_client:get_block([Peer], Hash),
 			case MaybeB of
 				Atom when is_atom(Atom) ->
-					ar:info([
-						"Failed to fetch block from peer. Will retry using a different one.",
-						{reply, Atom}
-					]),
+					ar:console(
+						"Failed to fetch block from peer. Will retry using a different one.~n"
+					),
+					?LOG_WARNING([{event, failed_to_fetch_block}]),
 					Atom;
 				B ->
 					{B, BI}
 			end
 	catch
 		Exc:Reason ->
-			ar:info([
-				"Failed to fetch block from peer. Will retry using a different one.",
-				{peer, Peer},
-				{exception, Exc},
-				{reason, Reason}
-			]),
+			ar:console(
+				"Failed to fetch block from peer ~s. Will retry using a different one.~n",
+				[ar_util:format_peer(peer)]
+			),
+			?LOG_WARNING([{event, failed_to_fetch_block}, {exception, Exc}, {reason, Reason}]),
 			find_current_block(Tail)
-	end.
-
-%% @doc Verify peers are on the same network as us and the clocks are in sync.
-filter_peer_list(Peers) ->
-	SameNetworkPeers =
-		lists:filter(
-			fun(Peer) ->
-				ar_http_iface_client:get_info(Peer, name) == <<?NETWORK_NAME>>
-			end,
-			Peers
-		),
-	case verify_time_sync(SameNetworkPeers) of
-		false ->
-			[];
-		true ->
-			SameNetworkPeers
 	end.
 
 join_peers(Peers) ->
@@ -171,6 +104,7 @@ join_peers(Peers) ->
 %% can validate transactions even if it enters a ?MAX_TX_ANCHOR_DEPTH-deep
 %% fork recovery (which is the deepest fork recovery possible) immediately after
 %% joining the network.
+%% @end
 get_block_and_trail(Peers, NewB, BI) ->
 	get_block_and_trail(Peers, NewB, 2 * ?MAX_TX_ANCHOR_DEPTH, BI).
 
@@ -189,11 +123,8 @@ get_block_and_trail(Peers, NewB, BehindCurrent, BI) ->
 			[NewB#block{ size_tagged_txs = SizeTaggedTXs } |
 				get_block_and_trail(Peers, PreviousBlock, BehindCurrent - 1, BI)];
 		false ->
-			ar:info(
-				[
-					could_not_retrieve_joining_block,
-					retrying
-				]
+			?LOG_INFO(
+				[{event, could_not_retrieve_joining_block}]
 			),
 			timer:sleep(3000),
 			get_block_and_trail(Peers, NewB, BehindCurrent, BI)
@@ -204,26 +135,26 @@ basic_node_join_test() ->
 	{timeout, 60, fun() ->
 		ar_storage:clear(),
 		[B0] = ar_weave:init([]),
-		{Node, _} = ar_test_node:start(B0),
-		ar_node:mine(Node),
-		ar_test_node:wait_until_height(Node, 1),
-		ar_node:mine(Node),
-		ar_test_node:wait_until_height(Node, 2),
-		Node2 = ar_test_node:join_on_master(),
-		ar_test_node:assert_slave_wait_until_height(Node2, 2)
+		ar_test_node:start(B0),
+		ar_node:mine(),
+		ar_test_node:wait_until_height(1),
+		ar_node:mine(),
+		ar_test_node:wait_until_height(2),
+		ar_test_node:join_on_master(),
+		ar_test_node:assert_slave_wait_until_height(2)
 	end}.
 
 %% @doc Ensure that both nodes can mine after a join.
 node_join_test() ->
 	{timeout, 60, fun() ->
 		[B0] = ar_weave:init([]),
-		Node1 = ar_test_node:start(B0),
-		ar_node:mine(Node1),
-		ar_test_node:wait_until_height(Node1, 1),
-		ar_node:mine(Node1),
-		ar_test_node:wait_until_height(Node1, 2),
-		Node2 = ar_test_node:join_on_master(),
-		ar_test_node:assert_slave_wait_until_height(Node2, 2),
-		ar_test_node:slave_mine(Node2),
-		ar_test_node:wait_until_height(Node1, 3)
+		ar_test_node:start(B0),
+		ar_node:mine(),
+		ar_test_node:wait_until_height(1),
+		ar_node:mine(),
+		ar_test_node:wait_until_height(2),
+		ar_test_node:join_on_master(),
+		ar_test_node:assert_slave_wait_until_height(2),
+		ar_test_node:slave_mine(),
+		ar_test_node:wait_until_height(3)
 	end}.

commit 64df69803113dcfa422bdc0ee8b81222cfe96694

Fix on-join error handling regression from f8f4771

apps/arweave/src/ar_join.erl
@@ -82,7 +82,7 @@ find_current_block([Peer | Tail]) ->
 		Exc:Reason ->
 			ar:console(
 				"Failed to fetch block from peer ~s. Will retry using a different one.~n",
-				[ar_util:format_peer(peer)]
+				[ar_util:format_peer(Peer)]
 			),
 			?LOG_WARNING([{event, failed_to_fetch_block}, {exception, Exc}, {reason, Reason}]),
 			find_current_block(Tail)