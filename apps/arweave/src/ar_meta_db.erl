ArweaveTeam/arweave

commit f8f4771b465d09e92a59096344aba31b2ab7cff6

Refactor node startup, ar_bridge, state management
The main motivation is to improve maintainability of the code. Below is a detailed list of changes.

- Public ETS tables are initialized in ar_sup, their lifecycle is not bound to that of the corresponding processes;
- all supervisors are launched in ar_sup; there it is very clear which processes are run and what is the dependency graph;
- process-specific initialization takes place in the corresponding init functions making ar:start clean;
- accounts (wallets) are initialized asynchronously;
- trusted peers are fetched directly from the application config instead of ar_meta_db;
- ar_bridge is a gen_server;
- ar_bridge does not depend on ar_node on startup anymore - ar_node_worker now loads and processes persisted mempool;
- ar_node does not manage the state copy any longer - state is stored in ETS; it simplifies the startup and saves us from the need to synchronize the state across two processes;
- OTP logger is used instead of the outdated error_logger;
- log format is changed; logs are one-liners now;
- logs are rotated;
- the polling configuration parameter is an integer now.

The change comes with the tests for webhooks, ar_poller, and persisted mempool.

Co-authored-by: Lev Berman <ldmberman@protonmail.com>

apps/arweave/src/ar_meta_db.erl
@@ -5,18 +5,22 @@
 
 -module(ar_meta_db).
 -behaviour(gen_server).
--define(SERVER, ?MODULE).
 
 -compile({no_auto_import, [{get, 1}, {put, 2}]}).
+
 -include_lib("eunit/include/eunit.hrl").
--include("ar.hrl").
 
-%% API
--export([start_link/0, stop/0, stop/1]).
--export([reset/0, reset_peer/1]).
--export([get/1, put/2, keys/0, purge_peer_performance/0, increase/2]).
+-include_lib("arweave/include/ar.hrl").
+-include_lib("arweave/include/ar_config.hrl").
 
-%% Behaviour callbacks
+-export([
+	start_link/0, stop/0, stop/1,
+	reset/0, reset_peer/1,
+	get/1, put/2, keys/0,
+	update_peer_performance/3, purge_peer_performance/0,
+	increase/2
+]).
+
 -export([
 	init/1,
 	handle_call/3,
@@ -30,36 +34,38 @@
 %% API
 %%------------------------------------------------------------------------------
 
-%% @doc Starts the server
+%% @doc Start the server.
 start_link() ->
-	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-%% @doc Stops the server
+%% @doc Stop the server.
 stop() ->
-	gen_server:stop(?SERVER).
+	gen_server:stop(?MODULE).
 
-%% @doc Stops the server with a reason
+%% @doc Stop the server with a reason.
 stop(Reason) ->
 	StopTimeout = 5000, %% milliseconds
-	gen_server:stop(?SERVER, Reason, StopTimeout).
+	gen_server:stop(?MODULE, Reason, StopTimeout).
 
-%% @doc Deletes all objects in db
+%% @doc Delete all objects in db.
 reset() ->
-	gen_server:call(?SERVER, reset).
+	gen_server:call(?MODULE, reset).
 
 reset_peer(Peer) ->
-	gen_server:call(?SERVER, {reset_peer, Peer}).
+	gen_server:call(?MODULE, {reset_peer, Peer}).
 
-%% @doc Insert key-value-pair into db
+%% @doc Insert key-value-pair into db.
 put(Key, Value) ->
-	gen_server:call(?SERVER, {put, Key, Value}).
+	%% Put an Erlang term into the meta DB. Typically these are write-once values.
+	ets:insert(?MODULE, {Key, Value}).
 
-%% @doc Retreive value for key
+%% @doc Retreive value for key.
 %% We don't want to serialize reads. So, we let
 %% the calling process do the ets:lookup directly
 %% without message passing. This is consistent with
 %% how the ets table is created (public and
 %% {read_concurrency, true}).
+%% @end
 get(Key) ->
 	case ets:lookup(?MODULE, Key) of
 		[{Key, Obj}] -> Obj;
@@ -68,55 +74,116 @@ get(Key) ->
 
 %% @doc Increase the value associated with Key by Val. If the key
 %% is not set, set it to Val.
+%% @end
 increase(Key, Val) ->
-	gen_server:cast(?SERVER, {increase, Key, Val}).
+	gen_server:cast(?MODULE, {increase, Key, Val}).
 
-%% @doc Remove entries from the performance database older than ?PEER_TMEOUT
+update_peer_performance(Peer, Time, Bytes) ->
+	gen_server:cast(update_peer_performance, {Peer, Time, Bytes}).
+
+%% @doc Remove entries from the performance database older than ?PEER_TMEOUT.
 purge_peer_performance() ->
-	gen_server:call(?SERVER, purge_peer_performance).
+	gen_server:call(?MODULE, purge_peer_performance).
 
 %% @doc Return all of the keys available in the database.
 keys() ->
-	gen_server:call(?SERVER, keys).
+	gen_server:call(?MODULE, keys).
 
 %%------------------------------------------------------------------------------
 %% Behaviour callbacks
 %%------------------------------------------------------------------------------
 
-%% @hidden
 init(_) ->
+	?LOG_INFO([{event, ar_meta_db_start}]),
 	%% Initialise the metadata storage service.
-	ar:report([starting_meta_db]),
-	ets:new(?MODULE, [set, public, named_table, {read_concurrency, true}]),
-	ets:new(blacklist, [set, public, named_table]),
+	{ok, Config} = application:get_env(arweave, config),
+	ets:insert(?MODULE, {data_dir, Config#config.data_dir}),
+	ets:insert(?MODULE, {metrics_dir, Config#config.metrics_dir}),
+	ets:insert(?MODULE, {port, Config#config.port}),
+	ets:insert(?MODULE, {disk_space, undefined}),
+	ets:insert(?MODULE, {used_space, Config#config.used_space}),
+	ets:insert(?MODULE, {mine, Config#config.mine}),
+	ets:insert(?MODULE, {max_miners, Config#config.max_miners}),
+	ets:insert(?MODULE, {max_emitters, Config#config.max_emitters}),
+	ets:insert(?MODULE,
+		{tx_propagation_parallelization, Config#config.tx_propagation_parallelization}),
+	ets:insert(?MODULE,
+		{transaction_blacklist_files, Config#config.transaction_blacklist_files}),
+	ets:insert(?MODULE, {transaction_blacklist_urls, Config#config.transaction_blacklist_urls}),
+	ets:insert(?MODULE,
+		{transaction_whitelist_files, Config#config.transaction_whitelist_files}),
+	ets:insert(?MODULE, {transaction_whitelist_urls, Config#config.transaction_whitelist_urls}),
+	ets:insert(?MODULE, {internal_api_secret, Config#config.internal_api_secret}),
+	ets:insert(?MODULE, {requests_per_minute_limit, Config#config.requests_per_minute_limit}),
+	ets:insert(?MODULE, {max_propagation_peers, Config#config.max_propagation_peers}),
+	ets:insert(?MODULE, {max_poa_option_depth, Config#config.max_poa_option_depth}),
+	ets:insert(?MODULE,
+		{disk_pool_data_root_expiration_time_us,
+			Config#config.disk_pool_data_root_expiration_time * 1000000}),
+	ets:insert(?MODULE, {max_disk_pool_buffer_mb, Config#config.max_disk_pool_buffer_mb}),
+	ets:insert(?MODULE,
+		{max_disk_pool_data_root_buffer_mb, Config#config.max_disk_pool_data_root_buffer_mb}),
+	ets:insert(?MODULE,
+		{randomx_bulk_hashing_iterations, Config#config.randomx_bulk_hashing_iterations}),
+	%% Store enabled features.
+	lists:foreach(
+		fun(Feature) ->
+			ets:insert(?MODULE, {Feature, true})
+		end,
+		Config#config.enable
+	),
+	lists:foreach(
+		fun(Feature) ->
+			ets:insert(?MODULE, {Feature, false})
+		end,
+		Config#config.disable
+	),
+	%% Prepare the storage for operation.
+	spawn(fun() -> ar_storage:init() end),
+	%% Optionally clear the block cache.
+	if Config#config.clean -> ar_storage:clear(); true -> do_nothing end,
+	ar_storage:start_update_used_space(),
 	{ok, #{}}.
 
-%% @hidden
 handle_call(reset, _From, State) ->
 	ets:delete_all_objects(?MODULE),
 	{reply, true, State};
-%% @hidden
+
 handle_call({reset_peer, Peer}, _From, State) ->
 	ets:delete(?MODULE, {peer, Peer}),
 	{reply, true, State};
-handle_call({put, Key, Val}, _From, State) ->
-	%% Put an Erlang term into the meta DB. Typically these are write-once values.
-	ets:insert(?MODULE, {Key, Val}),
-	{reply, true, State};
+
 handle_call(purge_peer_performance, _From, State) ->
 	purge_performance(),
 	{reply, ok, State};
+
 handle_call(keys, _From, State) ->
 	Keys = ets:foldl(fun collect_keys/2, [], ?MODULE),
 	{reply, Keys, State}.
 
-%% @hidden
+handle_cast({update_peer_performance, Peer, Time, Bytes}, State) ->
+	P =
+		case ets:lookup(?MODULE, {peer, Peer}) of
+			[{_, Performance}] ->
+				Performance;
+			[] ->
+				#performance{}
+		end,
+	ets:insert(?MODULE, {{peer, Peer},
+		P#performance{
+			transfers = P#performance.transfers + 1,
+			time = P#performance.time + Time,
+			bytes = P#performance.bytes + Bytes
+		}}),
+	{noreply, State};
+
 handle_cast({increase, Key, Val}, State) ->
 	case ets:lookup(?MODULE, Key) of
 		[{Key, PrevVal}] -> ets:insert(?MODULE, {Key, PrevVal + Val});
 		[] -> ets:insert(?MODULE, {Key, Val})
 	end,
 	{noreply, State};
+
 handle_cast(_What, State) ->
 	{noreply, State}.
 

commit 1a021d1fc8516126bcc6a93f207e4f363ceae06e

Fix disk_space flag broken in f8f4771b

apps/arweave/src/ar_meta_db.erl
@@ -100,7 +100,7 @@ init(_) ->
 	ets:insert(?MODULE, {data_dir, Config#config.data_dir}),
 	ets:insert(?MODULE, {metrics_dir, Config#config.metrics_dir}),
 	ets:insert(?MODULE, {port, Config#config.port}),
-	ets:insert(?MODULE, {disk_space, undefined}),
+	ets:insert(?MODULE, {disk_space, Config#config.disk_space}),
 	ets:insert(?MODULE, {used_space, Config#config.used_space}),
 	ets:insert(?MODULE, {mine, Config#config.mine}),
 	ets:insert(?MODULE, {max_miners, Config#config.max_miners}),