6742470c181c8b033641af16a0461c4c0d5d3215
James Piechota <piechota@gmail.com>
2023-03-24 15:41:20 +0000

    Spaces -> tabs

--- apps/arweave/src/ar_process_sampler.erl
+++ apps/arweave/src/ar_process_sampler.erl
@@ -10,43 +10,43 @@
 
 %% API
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% gen_server callbacks
 init([]) ->
-  timer:send_interval(?SAMPLE_INTERVAL, sample),
-  {ok, #{}}.
+	timer:send_interval(?SAMPLE_INTERVAL, sample),
+	{ok, #{}}.
 
 handle_call(_Request, _From, State) ->
-  {reply, ok, State}.
+	{reply, ok, State}.
 
 handle_cast(_Msg, State) ->
-  {noreply, State}.
+	{noreply, State}.
 
 handle_info(sample, State) ->
-  Processes = erlang:processes(),
-  ProcessNames = lists:filtermap(fun(Pid) -> process_function(Pid) end, Processes),
-  lists:foreach(fun(Name) -> prometheus_counter:inc(process_functions, [Name]) end, ProcessNames),
-  {noreply, State};
+	Processes = erlang:processes(),
+	ProcessNames = lists:filtermap(fun(Pid) -> process_function(Pid) end, Processes),
+	lists:foreach(fun(Name) -> prometheus_counter:inc(process_functions, [Name]) end, ProcessNames),
+	{noreply, State};
 
 handle_info(_Info, State) ->
-  {noreply, State}.
+	{noreply, State}.
 
 terminate(_Reason, _State) ->
-  ok.
+	ok.
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+	{ok, State}.
 
 %% Internal functions
 process_function(Pid) ->
-  case process_info(Pid, [current_function, status]) of
-    [{current_function, {?MODULE, process_function, _A}}, _] ->
-      false;
-    [{current_function, {erlang, process_info, _A}}, _] ->
-      false;
-    [{current_function, {M, F, A}}, {status, running}] ->
-      {true, atom_to_list(M) ++ ":" ++ atom_to_list(F) ++ "/" ++ integer_to_list(A)};
-    _ ->
-      false
-  end.
+	case process_info(Pid, [current_function, status]) of
+	[{current_function, {?MODULE, process_function, _A}}, _] ->
+		false;
+	[{current_function, {erlang, process_info, _A}}, _] ->
+		false;
+	[{current_function, {M, F, A}}, {status, running}] ->
+		{true, atom_to_list(M) ++ ":" ++ atom_to_list(F) ++ "/" ++ integer_to_list(A)};
+	_ ->
+		false
+	end.
