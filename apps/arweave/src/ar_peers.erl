cf17ed7126c7a6aaad59f07a94e8b7c4fea1bfe1
James Piechota <piechota@gmail.com>
2023-06-30 12:57:20 +0000

    print the peer stats after ranking

--- apps/arweave/src/ar_peers.erl
+++ apps/arweave/src/ar_peers.erl
@@ -203,7 +203,6 @@ stats(Peers) ->
 			Peers).
 
 discover_peers() ->
-	stats(),
 	case ets:lookup(?MODULE, peers) of
 		[] ->
 			ok;
@@ -295,6 +294,7 @@ handle_cast(rank_peers, State) ->
 	prometheus_gauge:set(arweave_peer_count, length(Peers)),
 	ets:insert(?MODULE, {peers, lists:sublist(rank_peers(Peers), ?MAX_PEERS)}),
 	ar_util:cast_after(?RANK_PEERS_FREQUENCY_MS, ?MODULE, rank_peers),
+	stats(),
 	{noreply, State};
 
 handle_cast(ping_peers, State) ->
