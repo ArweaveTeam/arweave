1bb198934f98b20b280aaf03ad7d7e5e7e988ff3
James Piechota <piechota@gmail.com>
2023-07-05 18:28:11 +0000

    WIP moving to {Peer, Metric, Performance} format

--- apps/arweave/include/ar_peers.hrl
+++ apps/arweave/include/ar_peers.hrl
@@ -5,21 +5,16 @@
 
 -define(STARTING_LATENCY_EMA, 1000). %% initial value to avoid over-weighting the first response
 
--record(metrics, {
+-define(AVAILABLE_METRICS, [overall, data_sync]). %% the performance metrics currently tracked
+
+-record(performance, {
+	version = 3,
+	release = -1,
 	bytes = 0,
 	latency = ?STARTING_LATENCY_EMA,
 	transfers = 0,
 	success = 1.0,
 	rating = 0
-}).
-
--record(performance, {
-	version = 3,
-	release = -1,
-	metrics = #{
-		overall => #metrics{},
-		data_sync => #metrics{}
-	}
 }).
 
 -endif.
