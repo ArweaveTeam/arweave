bc76887c0223dadc80b0201d7a9528e94fc8a301
James Piechota <piechota@gmail.com>
2023-09-03 18:42:09 +0000

    Add the testnet scripts to this branch

---
+++ testnet/assert_testnet.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ARWEAVE_DIR="$(readlink -f "$(dirname "$0")")/.."
+
+source $ARWEAVE_DIR/testnet/testnet_miners.sh
+source $ARWEAVE_DIR/testnet/testnet_vdf.sh
+
+# Get the current hostname
+current_host=$(hostname -f)
+
+# Check if current hostname is in the list of testnet servers
+is_testnet_server=0
+for server in "${TESTNET_SERVERS[@]}"; do
+    # Extract subdomain from the server in the list
+    subdomain="${server%%.*}"
+
+    if [[ "$current_host" == "$subdomain" ]]; then
+        is_testnet_server=1
+        break
+    fi
+done
+
+# If not a testnet server, abort
+if [[ "$is_testnet_server" -eq 0 ]]; then
+    exit 1
+fi
+
+mkdir -p /arweave-build/mainnet
+mkdir -p /arweave-build/testnet
---
+++ testnet/start_testnet_pilot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ARWEAVE_DIR="$(readlink -f "$(dirname "$0")")/.."
+
+if ! $ARWEAVE_DIR/testnet/assert_testnet.sh; then
+	echo "Error: This script must be run on a testnet server."
+	exit 1
+fi
+
+if [[ ! -f "/arweave-build/testnet/bin/start" ]]; then
+    echo "Arweave start script not found. Please run rebuild_testnet.sh first."
+	exit 1
+fi
+
+screen_cmd="screen -dmsL arweave /arweave-build/testnet/bin/start"
+screen_cmd+=$($ARWEAVE_DIR/testnet/build_data_flags.sh)
+screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh vdf_client_peer testnet_miners)
+
+screen_cmd+=" debug mine \
+data_dir /arweave-data \
+header_sync_jobs 0 start_from_block_index"
+
+echo "$screen_cmd"
+echo "$screen_cmd" > /arweave-build/testnet/run.command
+
+cd /arweave-build/testnet
+
+eval "$screen_cmd"
