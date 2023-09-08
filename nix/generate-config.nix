0cf8bede9b2b8bd54248682c0229d7600ee916e8
Hlöðver Sigurðsson <hlolli@gmail.com>
2023-09-08 18:14:08 +0200

    feat(nix): decouple arweave config generator

---
+++ nix/generate-config.nix
@@ -0,0 +1,58 @@
+{ arweaveConfig, pkgs, ... }:
+
+let
+  filterTopLevelNulls = set:
+    let
+      isNotNull = value: value != null;
+    in
+    filterAttrs (name: value: isNotNull value) set;
+in
+pkgs.writeText "config.json" (builtins.toJSON (filterTopLevelNulls {
+  data_dir = arweaveConfig.dataDir;
+  log_dir = arweaveConfig.logDir;
+  storage_modules = arweaveConfig.storageModules;
+  metrics_dir = arweaveConfig.metricsDir;
+  start_from_block_index = arweaveConfig.startFromBlockIndex;
+  transaction_blacklists = arweaveConfig.transactionBlacklists;
+  transaction_whitelists = arweaveConfig.transactionWhitelists;
+  transaction_blacklist_urls = arweaveConfig.transactionBlacklistURLs;
+  max_disk_pool_buffer_mb = arweaveConfig.maxDiskPoolBufferMb;
+  max_disk_pool_data_root_buffer_mb = arweaveConfig.maxDiskPoolDataRootBufferMb;
+  max_nonce_limiter_validation_thread_count = arweaveConfig.maxVDFValidationThreadCount;
+  block_pollers = arweaveConfig.blockPollers;
+  polling = arweaveConfig.polling;
+  tx_validators = arweaveConfig.txValidators;
+  disable = arweaveConfig.featuresDisable;
+  enable = arweaveConfig.featuresEnable;
+  header_sync_jobs = arweaveConfig.headerSyncJobs;
+  sync_jobs = arweaveConfig.syncJobs;
+  disk_pool_jobs = arweaveConfig.diskPoolJobs;
+  debug = arweaveConfig.debug;
+  packing_rate = arweaveConfig.packingRate;
+  block_throttle_by_ip_interval = arweaveConfig.blockThrottleByIPInterval;
+  block_throttle_by_solution_interval = arweaveConfig.blockThrottleBySolutionInterval;
+  semaphores = {
+    get_chunk = arweaveConfig.maxParallelGetChunkRequests;
+    get_and_pack_chunk = arweaveConfig.maxParallelGetAndPackChunkRequests;
+    get_tx_data = arweaveConfig.maxParallelGetTxDataRequests;
+    post_chunk = arweaveConfig.maxParallelPostChunkRequests;
+    get_block_index = arweaveConfig.maxParallelBlockIndexRequests;
+    get_wallet_list = arweaveConfig.maxParallelWalletListRequests;
+    get_sync_record = arweaveConfig.maxParallelGetSyncRecord;
+    arql = 10;
+    gateway_arql = 10;
+  };
+  requests_per_minute_limit = arweaveConfig.requestsPerMinuteLimit;
+  max_connections = arweaveConfig.maxConnections;
+
+  requests_per_minute_limit_by_ip = lib.lists.foldr
+    (ipObj: acc: acc // {
+      "${ipObj.ip}" = {
+        chunk = ipObj.chunkLimit;
+        data_sync_record = ipObj.dataSyncRecordLimit;
+        default = ipObj.defaultLimit;
+      };
+    })
+    { }
+    arweaveConfig.requestsPerMinuteLimitByIp;
+}));
--- nix/module.nix
+++ nix/module.nix
@@ -9,54 +9,8 @@ let
     let
       isNotNull = value: value != null;
     in
-      filterAttrs (name: value: isNotNull value) set;
-  generatedConfigFile =
-    pkgs.writeText "config.json" (builtins.toJSON (filterTopLevelNulls {
-      data_dir = cfg.dataDir;
-      log_dir = cfg.logDir;
-      storage_modules = cfg.storageModules;
-      metrics_dir = cfg.metricsDir;
-      start_from_block_index = cfg.startFromBlockIndex;
-      transaction_blacklists = cfg.transactionBlacklists;
-      transaction_whitelists = cfg.transactionWhitelists;
-      transaction_blacklist_urls = cfg.transactionBlacklistURLs;
-      max_disk_pool_buffer_mb = cfg.maxDiskPoolBufferMb;
-      max_disk_pool_data_root_buffer_mb = cfg.maxDiskPoolDataRootBufferMb;
-      max_nonce_limiter_validation_thread_count = cfg.maxVDFValidationThreadCount;
-      block_pollers = cfg.blockPollers;
-      polling = cfg.polling;
-      tx_validators = cfg.txValidators;
-      disable = cfg.featuresDisable;
-      enable = cfg.featuresEnable;
-      header_sync_jobs = cfg.headerSyncJobs;
-      sync_jobs = cfg.syncJobs;
-      disk_pool_jobs = cfg.diskPoolJobs;
-      debug = cfg.debug;
-      packing_rate = cfg.packingRate;
-      block_throttle_by_ip_interval = cfg.blockThrottleByIPInterval;
-      block_throttle_by_solution_interval = cfg.blockThrottleBySolutionInterval;
-      semaphores = {
-        get_chunk = cfg.maxParallelGetChunkRequests;
-        get_and_pack_chunk = cfg.maxParallelGetAndPackChunkRequests;
-        get_tx_data = cfg.maxParallelGetTxDataRequests;
-        post_chunk = cfg.maxParallelPostChunkRequests;
-        get_block_index = cfg.maxParallelBlockIndexRequests;
-        get_wallet_list = cfg.maxParallelWalletListRequests;
-        get_sync_record = cfg.maxParallelGetSyncRecord;
-        arql = 10;
-        gateway_arql = 10;
-      };
-      requests_per_minute_limit = cfg.requestsPerMinuteLimit;
-      max_connections = cfg.maxConnections;
-
-      requests_per_minute_limit_by_ip = lib.lists.foldr (ipObj: acc: acc // {
-        "${ipObj.ip}" = {
-          chunk = ipObj.chunkLimit;
-          data_sync_record = ipObj.dataSyncRecordLimit;
-          default = ipObj.defaultLimit;
-        };
-      }) {} cfg.requestsPerMinuteLimitByIp;
-    }));
+    filterAttrs (name: value: isNotNull value) set;
+  generatedConfigFile = "${import ./generate-config.nix { arweaveConfig = cfg; inherit pkgs; }}/config.json";
 in
 {
   options.services.arweave = {
@@ -357,7 +311,7 @@ in
           };
         };
       });
-      default = [];
+      default = [ ];
       description = "A rate limiter to prevent the node from receiving too many http requests over 1 minute period.";
     };
 
@@ -377,35 +331,37 @@ in
   };
 
   config = mkIf cfg.enable (
-    let screen-watchdog = pkgs.writeScriptBin "arweave-watch-screen" ''
-          #!${pkgs.bash}/bin/bash
-          while true
-          do
-            if ! ${pkgs.screen}/bin/screen -ls | grep -E -q "[0-9]\.arweave"; then
-              echo "arweace screen socket not detected, starting..."
-              ${pkgs.screen}/bin/screen -dmS arweave -L -Logfile /var/lib/arweave/logs/arweave@screen-session.txt &
-            fi
-            sleep 1
-          done
-        '';
+    let
+      screen-watchdog = pkgs.writeScriptBin "arweave-watch-screen" ''
+        #!${pkgs.bash}/bin/bash
+        while true
+        do
+          if ! ${pkgs.screen}/bin/screen -ls | grep -E -q "[0-9]\.arweave"; then
+            echo "arweace screen socket not detected, starting..."
+            ${pkgs.screen}/bin/screen -dmS arweave -L -Logfile /var/lib/arweave/logs/arweave@screen-session.txt &
+          fi
+          sleep 1
+        done
+      '';
 
-        arweave-service-pre-start = pkgs.writeScriptBin "arweave-pre-start" ''
-          #!${pkgs.bash}/bin/bash
-          # wait for screen socket for S-arweave to appear
-          until [ "$(${pkgs.screen}/bin/screen -ls | grep 'S-arweave')" ]
-          do
-            sleep 1
-          done
-          exit 0
-        '';
+      arweave-service-pre-start = pkgs.writeScriptBin "arweave-pre-start" ''
+        #!${pkgs.bash}/bin/bash
+        # wait for screen socket for S-arweave to appear
+        until [ "$(${pkgs.screen}/bin/screen -ls | grep 'S-arweave')" ]
+        do
+          sleep 1
+        done
+        exit 0
+      '';
 
-        arweave-service-start =
-          let
-            command = "${cfg.package}/bin/start-nix config_file ${cfg.configFile}";
-            peers = "${builtins.concatStringsSep " " (builtins.concatMap (p: ["peer" p]) cfg.peer)}";
-            vdf-peers = "${builtins.concatStringsSep " " (builtins.concatMap (p: ["vdf_client_peer" p]) cfg.vdfClientPeer)}";
-            vdf-server-peers = "${builtins.concatStringsSep " " (builtins.concatMap (p: ["vdf_server_trusted_peer" p]) cfg.vdfServerTrustedPeer)}";
-          in pkgs.writeScriptBin "arweave-start" ''
+      arweave-service-start =
+        let
+          command = "${cfg.package}/bin/start-nix config_file ${cfg.configFile}";
+          peers = "${builtins.concatStringsSep " " (builtins.concatMap (p: ["peer" p]) cfg.peer)}";
+          vdf-peers = "${builtins.concatStringsSep " " (builtins.concatMap (p: ["vdf_client_peer" p]) cfg.vdfClientPeer)}";
+          vdf-server-peers = "${builtins.concatStringsSep " " (builtins.concatMap (p: ["vdf_server_trusted_peer" p]) cfg.vdfServerTrustedPeer)}";
+        in
+        pkgs.writeScriptBin "arweave-start" ''
           #!${pkgs.bash}/bin/bash
           ${pkgs.screen}/bin/screen -S arweave -p 0 -X stuff "^C^M" || true
           ${pkgs.screen}/bin/screen -S arweave -p 0 -X stuff '${command} ${peers} ${vdf-peers} ${vdf-server-peers}^M'
@@ -416,31 +372,32 @@ in
           done
         '';
 
-        arweave-service-stop = pkgs.writeScriptBin "arweave-stop" ''
-          #!${pkgs.bash}/bin/bash
-          ${cfg.package}/bin/stop-nix || true
-        '';
+      arweave-service-stop = pkgs.writeScriptBin "arweave-stop" ''
+        #!${pkgs.bash}/bin/bash
+        ${cfg.package}/bin/stop-nix || true
+      '';
 
-        arweave-service-stop-post = pkgs.writeScriptBin "arweave-stop-post" ''
-          #!${pkgs.bash}/bin/bash
-          # wait for empd to die, otherwise kill it
-          counter=0
-          ${cfg.package}/bin/stop-nix || true
-          until [[ "$(${pkgs.procps}/bin/ps -C epmd &> /dev/null)" -ne 0 ]] || [[ "$counter" -gt 15 ]]
-          do
-            sleep 1
-            let counter++
-          done
-          [ "$(${pkgs.procps}/bin/ps -C epmd &> /dev/null)" ] || ${pkgs.procps}/bin/pkill epmd || true
-          exit 0
-        '';
+      arweave-service-stop-post = pkgs.writeScriptBin "arweave-stop-post" ''
+        #!${pkgs.bash}/bin/bash
+        # wait for empd to die, otherwise kill it
+        counter=0
+        ${cfg.package}/bin/stop-nix || true
+        until [[ "$(${pkgs.procps}/bin/ps -C epmd &> /dev/null)" -ne 0 ]] || [[ "$counter" -gt 15 ]]
+        do
+          sleep 1
+          let counter++
+        done
+        [ "$(${pkgs.procps}/bin/ps -C epmd &> /dev/null)" ] || ${pkgs.procps}/bin/pkill epmd || true
+        exit 0
+      '';
 
-    in {
+    in
+    {
 
       systemd.services.arweave-screen = {
         description = "A Service for starting Screen process";
         after = [ "network.target" ];
-        environment = {};
+        environment = { };
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
           User = cfg.user;
@@ -455,7 +412,7 @@ in
       systemd.services.arweave = {
         description = "Arweave Node Service";
         after = [ "arweave-screen.service" ];
-        environment = {};
+        environment = { };
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
           User = cfg.user;
@@ -469,5 +426,6 @@ in
           ExecStopPost = "${arweave-service-stop-post}/bin/arweave-stop-post";
         };
       };
-    });
+    }
+  );
 }
