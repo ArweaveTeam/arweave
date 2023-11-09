2686bcf4fe9f039afaf84593de21a83b116c3879
Lev Berman <ldmberman@proton.me>
2023-11-09 18:01:42 +0100

    Add VDF difficulty to the VDF session key so that a new session is created whenever VDF difficulty changes

--- apps/arweave/include/ar.hrl
+++ apps/arweave/include/ar.hrl
@@ -21,7 +21,7 @@
 -define(CLIENT_VERSION, 5).
 
 %% The current build number -- incremented for every release.
--define(RELEASE_NUMBER, 66).
+-define(RELEASE_NUMBER, 67).
 
 -define(DEFAULT_REQUEST_HEADERS,
 	[
