b9923add6d2286946be32e6ca1b500cc5383a2df
James Piechota <piechota@gmail.com>
2024-01-09 22:35:59 +0000

    Complete the process of "sharding" the mining out to mining workers This change makes all ar_mining_workers bound to a partitin instead of a session. Each worker tracks their active sessions *and* tracks the chunk cache and chunk cache size for their partition as well as any H2 work that happens off of their partition.
    This allows us to remove the ets table for tracking the chunk cache
    which removes one more centralized bottleneck when scaling out the
    number of partitions.

--- apps/arweave/src/ar_mining_sup.erl
+++ apps/arweave/src/ar_mining_sup.erl
@@ -21,24 +21,18 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-	{ok, Config} = application:get_env(arweave, config),
-	NumStorageModules = length(Config#config.storage_modules),
-	MaxWorkers = max(2, 2 * trunc((erlang:system_info(schedulers_online) - 1) / 2)),
-	%% Ideally we have 2 workers per storage module - 1 for the current VDF session and one
-	%% for the previous VDF session. However we don't want more workers than there are schedulers
-	%% so limit to largest multiple of 2 less than the number of schedulers (but no less than 2).
-	NumMiningWorkers = min(MaxWorkers, 2*NumStorageModules),
-	?LOG_INFO([{event, ar_mining_sup}, {num_mining_workers, NumMiningWorkers}]),
+	%% We'll create workers for all configured parititions - even those partitions that
+	%% currently exceed the weave size. Those workers will just lie dormant until the
+	%% weave size grows to meet them.
 	MiningWorkers = lists:map(
-		fun(Number) ->
-			Name = list_to_atom("ar_mining_worker_" ++ integer_to_list(Number)),
-			?CHILD_WITH_ARGS(ar_mining_worker, worker, Name, [Name])
+		fun({Partition, _}) ->
+			?CHILD_WITH_ARGS(
+				ar_mining_worker, worker, ar_mining_worker:name(Partition), [Partition])
 		end,
-		lists:seq(1, NumMiningWorkers)
+		ar_mining_io:get_partitions(infinity)
 	),
-	MiningWorkerNames = [element(1, El) || El <- MiningWorkers],
 	Children = MiningWorkers ++ [
-		?CHILD_WITH_ARGS(ar_mining_server, worker, ar_mining_server, [MiningWorkerNames]),
+		?CHILD(ar_mining_server, worker),
 		?CHILD(ar_mining_hash, worker),
 		?CHILD(ar_mining_io, worker),
 		?CHILD(ar_mining_stats, worker)
