dab83e8ee99e69a93cf92b23acb0499ac51e2bc1
James Piechota <piechota@gmail.com>
2024-03-04 22:51:50 +0000

    Script to download a partition from a list of URLs

--- bin/download-partition.sh
+++ bin/download-partition.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-set -x
-
-# Function to handle SIGINT (CTRL-C)
-cleanup() {
-  echo "Interrupt received. Cleaning up..."
-  # Kill all child processes of this script
-  pkill -P $$
-  exit 1
-}
-
-# Trap SIGINT and call the cleanup function
-trap cleanup SIGINT
-
 # Check if two arguments are passed
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <max_concurrent_downloads> <input_file>"
@@ -24,8 +11,15 @@ max_concurrent_downloads=$1
 # Second argument: Input file to read URLs from
 input_file=$2
 
+# Calculate the total number of lines (files to download) at the beginning
+total_lines=$(wc -l < "$input_file")
+echo "Total files to download: $total_lines"
+
 # Counter for the number of concurrent downloads
 concurrent_downloads=0
+
+# Counter for the number of lines processed
+lines_processed=0
 
 # Read each line from the input file
 while IFS= read -r line; do
@@ -35,7 +29,7 @@ while IFS= read -r line; do
   fi
 
   # Dynamically determine the base URL for the current line, extracting the first three segments
-  base_url=$(echo "$line" | cut -d'/' -f1-5)/
+  base_url=$(echo "$line" | cut -d'/' -f1-6)/
 
   # Remove the base URL to get the relative path
   relative_path="${line#$base_url}"
@@ -43,18 +37,27 @@ while IFS= read -r line; do
   # Create the directory structure for the file
   mkdir -p "$(dirname "$relative_path")"
 
+  echo "Downloading $line to ./$relative_path"
+
   # Download the file using wget in the background, preserving the directory structure
-  wget -c -O "$relative_path" "$line" &
+  wget -q -c -O "$relative_path" "$line" &
 
   # Increment the concurrent downloads counter
   ((concurrent_downloads++))
+  ((lines_processed++))
 
   # If the number of concurrent downloads reaches the max, wait for all to complete before continuing
   if (( concurrent_downloads >= max_concurrent_downloads )); then
     wait
     concurrent_downloads=0
+    # Calculate and print the remaining lines (files) to download
+    remaining_lines=$((total_lines - lines_processed))
+    echo "Remaining files to download: $remaining_lines"
   fi
 done < "$input_file" # Read URLs from the input file specified as the second argument
 
 # Wait for any remaining background downloads to complete
 wait
+# After the last batch of downloads, print the remaining lines one last time
+remaining_lines=$((total_lines - lines_processed))
+echo "Remaining files to download: $remaining_lines"
