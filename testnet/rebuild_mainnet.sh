33b9071a9968c1e0f227c2f3e5dcc9888dcd56ad
James Piechota <piechota@gmail.com>
2024-08-13 19:21:12 +0000

    Update testnet configuration for 2.8 testing

--- testnet/rebuild_mainnet.sh
+++ testnet/rebuild_mainnet.sh
@@ -13,7 +13,7 @@ rm -rf /arweave-build/mainnet/*
 echo "$0 $@" > /arweave-build/mainnet/build.command
 
 cd $ARWEAVE_DIR
-rm $ARWEAVE_DIR/_build/prod/rel/arweave/*.tar.gz
+rm -rf $ARWEAVE_DIR/_build/prod/rel/arweave/*
 $ARWEAVE_DIR/rebar3 as prod tar
 tar xf $ARWEAVE_DIR/_build/prod/rel/arweave/arweave-*.tar.gz -C /arweave-build/mainnet
 
