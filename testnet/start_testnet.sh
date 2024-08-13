e7be57f07c0af74f26cd0bed000e45fd97f01780
James Piechota <piechota@gmail.com>
2024-08-13 19:21:12 +0000

    Update testnet configuration for 2.8 testing

--- testnet/start_testnet.sh
+++ testnet/start_testnet.sh
@@ -20,46 +20,12 @@ if [[ ! -f "/arweave-build/testnet/bin/start" ]]; then
 fi
 
 node=$(hostname -f)
-source $ARWEAVE_DIR/testnet/testnet_nodes.sh
+config_file="$ARWEAVE_DIR/testnet/config/$(hostname -f).json"
+SCREEN_CMD="screen -dmsL arweave /arweave-build/testnet/bin/start config_file $config_file $*"
 
-screen_cmd="screen -dmsL arweave /arweave-build/testnet/bin/start"
-screen_cmd+=$($ARWEAVE_DIR/testnet/build_data_flags.sh)
-
-
-if is_node_in_array "$node" "${VDF_SERVER_NODES[@]}"; then
-    # VDF Server and Pilot node flags
-    screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh vdf_client_peer "${VDF_CLIENT_NODES[@]}")
-    if [ $# -eq 0 ]; then
-        screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh peer "${ALL_NODES[@]}")
-    else
-        screen_cmd+=" header_sync_jobs 0"
-    fi
-else
-    screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh peer "${ALL_NODES[@]}")
-    if is_node_in_array "$node" "${VDF_CLIENT_NODES[@]}"; then
-        # VDF Client node flags
-        screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh vdf_server_trusted_peer \
-            "${VDF_SERVER_NODES[@]}")
-    fi
-fi
-
-if is_node_in_array "$node" "${CM_EXIT_NODE[@]}"; then
-    screen_cmd+=" coordinated_mining cm_api_secret testnet_cm_secret"
-    screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh cm_peer "${CM_MINER_NODES[@]}")
-elif is_node_in_array "$node" "${CM_MINER_NODES[@]}"; then
-    screen_cmd+=" coordinated_mining cm_api_secret testnet_cm_secret"
-    screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh cm_exit_peer "${CM_EXIT_NODE[@]}")
-    screen_cmd+=$($ARWEAVE_DIR/testnet/build_peer_flags.sh cm_peer "${CM_MINER_NODES[@]}")
-fi
-
-
-
-screen_cmd+=" $* debug mine enable remove_orphaned_storage_module_data data_dir /arweave-data \
-requests_per_minute_limit 9000"
-
-echo "$screen_cmd"
-echo "$screen_cmd" > /arweave-build/testnet/run.command
+echo "$SCREEN_CMD"
+echo "$SCREEN_CMD" > /arweave-build/testnet/run.sh
+chmod +x /arweave-build/testnet/run.sh
 
 cd /arweave-build/testnet
-
-eval "$screen_cmd"
+./run.sh
