7bd59982b0e374a9fe8da7ba35ce1568465c1ea5
James Piechota <piechota@gmail.com>
2024-08-13 19:21:12 +0000

    Update testnet configuration for 2.8 testing

--- testnet/start_mainnet.sh
+++ testnet/start_mainnet.sh
@@ -12,38 +12,12 @@ if [[ ! -f "/arweave-build/mainnet/bin/start" ]]; then
 	exit 1
 fi
 
-SCREEN_CMD="screen -dmsL arweave /arweave-build/mainnet/bin/start"
-address=""
-
-# look in /arweave-data to determine this node's mining address and storage modules
-for dir in /arweave-data/storage_modules/storage_module_*; do
-	# get the storage_module directory names
-	filename=$(basename $dir)
-
-	# remove the leading storage_module_
-	prefix="storage_module_"
-	str="${filename#$prefix}"
-
-    # Extract size, index, and address
-    size=$(awk -F'_' '{print $1}' <<< "$str")
-	index=$(awk -F'_' '{print $2}' <<< "$str")
-	prefix="${size}_${index}_"
-	address="${str#$prefix}"
-    
-    # Append to the screen command
-    SCREEN_CMD="$SCREEN_CMD storage_module $index,$size,$address"
-done
-
-SCREEN_CMD="$SCREEN_CMD mining_addr $address \
-debug \
-data_dir /arweave-data \
-peer ams-1.eu-central-1.arweave.net \
-peer fra-1.eu-central-2.arweave.net \
-header_sync_jobs 1"
+config_file="$ARWEAVE_DIR/testnet/config/$(hostname -f).json"
+SCREEN_CMD="screen -dmsL arweave /arweave-build/mainnet/bin/start config_file $config_file"
 
 echo "$SCREEN_CMD"
-echo "$SCREEN_CMD" > /arweave-build/mainnet/run.command
+echo "$SCREEN_CMD" > /arweave-build/mainnet/run.sh
+chmod +x /arweave-build/mainnet/run.sh
 
 cd /arweave-build/mainnet
-
-eval "$SCREEN_CMD"
+./run.sh
