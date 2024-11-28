a8dfb4e5f46c3fd3a141103b624bfa0a157ba943
Denis Fakhrtdinov <denis@spawnlink.eu>
2024-11-28 13:43:02 +0200

    Add node introspection options (#651)

---
+++ apps/arweave/src/user_default.erl
@@ -0,0 +1,17 @@
+%%
+%% This file is loaded upon starting Erlang REPL, and loads all the records
+%% from user_default.hrl file.
+%% Another possibility is to add some broadly-user functions here: these
+%% functions will be useable from the REPL as first-class commands. As an
+%% example, running the `config().` in the REPL will return current node config.
+%%
+
+-module(user_default).
+-include_lib("arweave/include/user_default.hrl").
+-compile([export_all, nowarn_export_all]).
+
+
+
+config() ->
+  {ok, Config} = application:get_env(arweave, config),
+  Config.
