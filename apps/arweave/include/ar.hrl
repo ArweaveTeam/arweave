738b3495a2732f1d5f5b0f7433bcb313e33221fa
James Piechota <piechota@gmail.com>
2025-02-04 01:06:42 +0000

    Bump release number 79, 2.9.3

--- apps/arweave/include/ar.hrl
+++ apps/arweave/include/ar.hrl
@@ -27,7 +27,7 @@
 -define(CLIENT_VERSION, 5).
 
 %% The current build number -- incremented for every release.
--define(RELEASE_NUMBER, 78).
+-define(RELEASE_NUMBER, 79).
 
 -define(DEFAULT_REQUEST_HEADERS,
 	[
