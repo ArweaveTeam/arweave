54cef41d7326f1de66b87f0adc745a458a536ce9
Denis Fakhrtdinov <me@shizz.ru>
2025-02-26 19:22:33 +0200

    Apply various quick-n-dirty fixes for HTTP requests client and handlers

--- apps/arweave/src/ar_http.erl
+++ apps/arweave/src/ar_http.erl
@@ -66,44 +66,46 @@ req(Args) ->
 req(Args, ReestablishedConnection) ->
 	StartTime = erlang:monotonic_time(),
 	#{ peer := Peer, path := Path, method := Method } = Args,
-	Response = case catch gen_server:call(?MODULE, {get_connection, Args}, infinity) of
-		{ok, PID} ->
-			ar_rate_limiter:throttle(Peer, Path),
-			case request(PID, Args) of
-				{error, Error} when Error == {shutdown, normal}; Error == noproc ->
-					case ReestablishedConnection of
-						true ->
-							{error, client_error};
-						false ->
-							req(Args, true)
+	case catch ar_rate_limiter:throttle(Peer, Path) of
+		{'EXIT', _} -> {error, client_error};
+		ok ->
+			Response = case catch gen_server:call(?MODULE, {get_connection, Args}, 15000) of
+				{ok, PID} ->
+					case request(PID, Args) of
+						{error, Error} when Error == {shutdown, normal}; Error == noproc ->
+							case ReestablishedConnection of
+								true ->
+									{error, client_error};
+								false ->
+									req(Args, true)
+							end;
+						Reply ->
+							Reply
 					end;
-				Reply ->
-					Reply
-			end;
-		{'EXIT', _} ->
-			{error, client_error};
-		Error ->
-			Error
-	end,
-	EndTime = erlang:monotonic_time(),
-	%% Only log the metric for the top-level call to req/2 - not the recursive call
-	%% that happens when the connection is reestablished.
-	case ReestablishedConnection of
-		true ->
-			ok;
-		false ->
-			%% NOTE: the erlang prometheus client looks at the metric name to determine units.
-			%%       If it sees <name>_duration_<unit> it assumes the observed value is in
-			%%       native units and it converts it to <unit> .To query native units, use:
-			%%       erlant:monotonic_time() without any arguments.
-			%%       See: https://github.com/deadtrickster/prometheus.erl/blob/6dd56bf321e99688108bb976283a80e4d82b3d30/src/prometheus_time.erl#L2-L84
-			prometheus_histogram:observe(ar_http_request_duration_seconds, [
-					method_to_list(Method),
-					ar_http_iface_server:label_http_path(list_to_binary(Path)),
-					ar_metrics:get_status_class(Response)
-				], EndTime - StartTime)
-	end,
-	Response.
+				{'EXIT', _} -> {error, client_error};
+				Error -> Error
+			end,
+			EndTime = erlang:monotonic_time(),
+			%% Only log the metric for the top-level call to req/2 - not the recursive call
+			%% that happens when the connection is reestablished.
+			case ReestablishedConnection of
+				true ->
+					ok;
+				false ->
+					%% NOTE: the erlang prometheus client looks at the metric name to determine units.
+					%%       If it sees <name>_duration_<unit> it assumes the observed value is in
+					%%       native units and it converts it to <unit> .To query native units, use:
+					%%       erlant:monotonic_time() without any arguments.
+					%%       See: https://github.com/deadtrickster/prometheus.erl/blob/6dd56bf321e99688108bb976283a80e4d82b3d30/src/prometheus_time.erl#L2-L84
+					prometheus_histogram:observe(ar_http_request_duration_seconds, [
+							method_to_list(Method),
+							ar_http_iface_server:label_http_path(list_to_binary(Path)),
+							ar_metrics:get_status_class(Response)
+						], EndTime - StartTime)
+			end,
+			Response
+	end.
+
 %%% ==================================================================
 %%% gen_server callbacks.
 %%% ==================================================================
