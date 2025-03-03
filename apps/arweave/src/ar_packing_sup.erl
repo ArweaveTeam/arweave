2d205e13cc95c4a8e4e115916685195a6c9609d1
James Piechota <piechota@gmail.com>
2025-03-03 15:31:18 +0000

    feat: add data-doctor inspect
    Just a debugging tool to help determine what packing format was
    used on a chunk

---
+++ apps/arweave/src/ar_packing_sup.erl
@@ -0,0 +1,26 @@
+-module(ar_packing_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+-include_lib("arweave/include/ar_sup.hrl").
+
+%%%===================================================================
+%%% Public interface.
+%%%===================================================================
+
+start_link() ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks.
+%% ===================================================================
+
+init([]) ->
+    ets:new(ar_packing_server, [set, public, named_table]),
+	{ok, {{one_for_one, 5, 10}, [
+		?CHILD(ar_packing_server, worker)
+	]}}.
