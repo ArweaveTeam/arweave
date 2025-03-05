5e18770ab5e1cadb6e843f019b7be5beaba51bad
Denis Fakhrtdinov <me@shizz.ru>
2025-03-05 20:03:50 +0200

    Remove linger option

--- apps/arweave/src/ar_http_iface_server.erl
+++ apps/arweave/src/ar_http_iface_server.erl
@@ -71,7 +71,6 @@ start_http_iface_listener(Config) ->
 	TlsCertfilePath = Config#config.tls_cert_file,
 	TlsKeyfilePath = Config#config.tls_key_file,
 	TransportOpts = [
-		{linger, {true, 10}},
 		{port, Config#config.port},
 		{keepalive, true},
 		{max_connections, Config#config.max_connections}
