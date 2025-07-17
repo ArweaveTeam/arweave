a371366a22dc37b5f21c2a9a765d5dda5c5e5721
Lev Berman <ldmberman@proton.me>
2025-07-17 21:44:00 +0200

    Split ar_data_sync_tests into several modules
    Put each test in a separate module to ensure clean shutdown on CI.

---
+++ apps/arweave/test/ar_data_sync_enqueue_intervals_test.erl
@@ -0,0 +1,222 @@
+-module(ar_data_sync_enqueue_intervals_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("../include/ar.hrl").
+-include("../include/ar_consensus.hrl").
+-include("../include/ar_config.hrl").
+
+enqueue_intervals_test() ->
+	?LOG_DEBUG([{event, enqueue_intervals_test}]),
+	test_enqueue_intervals([], 2, [], [], [], "Empty Intervals"),
+	Peer1 = {1, 2, 3, 4, 1984},
+	Peer2 = {101, 102, 103, 104, 1984},
+	Peer3 = {201, 202, 203, 204, 1984},
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+					{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+					{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+				])}
+		],
+		5,
+		[{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE}],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+			{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, 4*?DATA_CHUNK_SIZE, Peer1},
+			{6*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE, Peer1},
+			{7*?DATA_CHUNK_SIZE, 8*?DATA_CHUNK_SIZE, Peer1},
+			{8*?DATA_CHUNK_SIZE, 9*?DATA_CHUNK_SIZE, Peer1}
+		],
+		"Single peer, full intervals, all chunks. Non-overlapping QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+					{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+					{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+				])}
+		],
+		2,
+		[{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE}],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, 4*?DATA_CHUNK_SIZE, Peer1}
+		],
+		"Single peer, full intervals, 2 chunks. Non-overlapping QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+			])},
+			{Peer2, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{7*?DATA_CHUNK_SIZE, 5*?DATA_CHUNK_SIZE}
+			])},
+			{Peer3, ar_intervals:from_list([
+				{8*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE}
+			])}
+		],
+		2,
+		[{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE}],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+			{8*?DATA_CHUNK_SIZE, 5*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, 4*?DATA_CHUNK_SIZE, Peer1},
+			{5*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE, Peer2},
+			{6*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE, Peer2},
+			{7*?DATA_CHUNK_SIZE, 8*?DATA_CHUNK_SIZE, Peer3}
+		],
+		"Multiple peers, overlapping, full intervals, 2 chunks. Non-overlapping QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+			])},
+			{Peer2, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{7*?DATA_CHUNK_SIZE, 5*?DATA_CHUNK_SIZE}
+			])},
+			{Peer3, ar_intervals:from_list([
+				{8*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE}
+			])}
+		],
+		3,
+		[{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE}],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+			{8*?DATA_CHUNK_SIZE, 5*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, 4*?DATA_CHUNK_SIZE, Peer1},
+			{5*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE, Peer2},
+			{6*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE, Peer1},
+			{7*?DATA_CHUNK_SIZE, 8*?DATA_CHUNK_SIZE, Peer3}
+		],
+		"Multiple peers, overlapping, full intervals, 3 chunks. Non-overlapping QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+					{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+					{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+			])}
+		],
+		5,
+		[{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE}, {9*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE}],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+			{7*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, 4*?DATA_CHUNK_SIZE, Peer1},
+			{6*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE, Peer1}
+		],
+		"Single peer, full intervals, all chunks. Overlapping QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{9*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+			])},
+			{Peer2, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{7*?DATA_CHUNK_SIZE, 5*?DATA_CHUNK_SIZE}
+			])},
+			{Peer3, ar_intervals:from_list([
+				{8*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE}
+			])}
+		],
+		2,
+		[{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE}, {9*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE}],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+			{7*?DATA_CHUNK_SIZE, 5*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, 4*?DATA_CHUNK_SIZE, Peer1},
+			{5*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE, Peer2},
+			{6*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE, Peer2}
+		],
+		"Multiple peers, overlapping, full intervals, 2 chunks. Overlapping QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+				{trunc(3.25*?DATA_CHUNK_SIZE), 2*?DATA_CHUNK_SIZE},
+				{9*?DATA_CHUNK_SIZE, trunc(5.75*?DATA_CHUNK_SIZE)}
+			])}
+		],
+		2,
+		[
+			{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE},
+			{trunc(8.5*?DATA_CHUNK_SIZE), trunc(6.5*?DATA_CHUNK_SIZE)}
+		],
+		[
+			{trunc(3.25*?DATA_CHUNK_SIZE), 2*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, trunc(3.25*?DATA_CHUNK_SIZE), Peer1}
+		],
+		"Single peer, partial intervals, 2 chunks. Overlapping partial QIntervals."),
+
+	test_enqueue_intervals(
+		[
+			{Peer1, ar_intervals:from_list([
+				{trunc(3.25*?DATA_CHUNK_SIZE), 2*?DATA_CHUNK_SIZE},
+				{9*?DATA_CHUNK_SIZE, trunc(5.75*?DATA_CHUNK_SIZE)}
+			])},
+			{Peer2, ar_intervals:from_list([
+				{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+				{7*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+			])},
+			{Peer3, ar_intervals:from_list([
+				{8*?DATA_CHUNK_SIZE, 7*?DATA_CHUNK_SIZE}
+			])}
+		],
+		2,
+		[
+			{20*?DATA_CHUNK_SIZE, 10*?DATA_CHUNK_SIZE},
+			{trunc(8.5*?DATA_CHUNK_SIZE), trunc(6.5*?DATA_CHUNK_SIZE)}
+		],
+		[
+			{4*?DATA_CHUNK_SIZE, 2*?DATA_CHUNK_SIZE},
+			{8*?DATA_CHUNK_SIZE, 6*?DATA_CHUNK_SIZE}
+		],
+		[
+			{2*?DATA_CHUNK_SIZE, 3*?DATA_CHUNK_SIZE, Peer1},
+			{3*?DATA_CHUNK_SIZE, trunc(3.25*?DATA_CHUNK_SIZE), Peer1},
+			{trunc(3.25*?DATA_CHUNK_SIZE), 4*?DATA_CHUNK_SIZE, Peer2},
+			{6*?DATA_CHUNK_SIZE, trunc(6.5*?DATA_CHUNK_SIZE), Peer2}
+		],
+		"Multiple peers, overlapping, full intervals, 2 chunks. Overlapping QIntervals.").
+
+test_enqueue_intervals(Intervals, ChunksPerPeer, QIntervalsRanges, ExpectedQIntervalRanges, ExpectedChunks, Label) ->
+	QIntervals = ar_intervals:from_list(QIntervalsRanges),
+	Q = gb_sets:new(),
+	{QResult, QIntervalsResult} = ar_data_sync:enqueue_intervals(Intervals, ChunksPerPeer, {Q, QIntervals}),
+	ExpectedQIntervals = lists:foldl(fun({End, Start}, Acc) ->
+			ar_intervals:add(Acc, End, Start)
+		end, QIntervals, ExpectedQIntervalRanges),
+	?assertEqual(ar_intervals:to_list(ExpectedQIntervals), ar_intervals:to_list(QIntervalsResult), Label),
+	?assertEqual(ExpectedChunks, gb_sets:to_list(QResult), Label). 
---
+++ apps/arweave/test/ar_data_sync_recovers_from_corruption_test.erl
@@ -0,0 +1,22 @@
+-module(ar_data_sync_recovers_from_corruption_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("../include/ar.hrl").
+-include("../include/ar_consensus.hrl").
+-include("../include/ar_config.hrl").
+
+-import(ar_test_node, [assert_wait_until_height/2]).
+
+recovers_from_corruption_test_() ->
+	{timeout, 300, fun test_recovers_from_corruption/0}.
+
+test_recovers_from_corruption() ->
+	?LOG_DEBUG([{event, test_recovers_from_corruption_start}]),
+	ar_test_data_sync:setup_nodes(),
+	StoreID = ar_storage_module:id(hd(ar_storage_module:get_all(262144 * 3))),
+	?debugFmt("Corrupting ~s...", [StoreID]),
+	[ar_chunk_storage:write_chunk(PaddedEndOffset, << 0:(262144*8) >>, #{}, StoreID)
+			|| PaddedEndOffset <- lists:seq(262144, 262144 * 3, 262144)],
+	ar_test_node:mine(),
+	ar_test_node:assert_wait_until_height(main, 1). 
