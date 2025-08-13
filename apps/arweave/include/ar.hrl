46d63b4fb6909b612452cf947a9d8f4b72ad3fa3
Lev Berman <ldmberman@proton.me>
2025-08-13 13:22:30 +0200

    Bump the release number for the future release
    Bumping the release number early allows test nodes to distinguish
    updated nodes from not updated notes and helps us avoid the bug
    where the code relies on the outdated release number (e.g., when
    querying freshly introduced endpoints)

--- apps/arweave/include/ar.hrl
+++ apps/arweave/include/ar.hrl
@@ -33,7 +33,7 @@
 -define(CLIENT_VERSION, 5).
 
 %% The current build number -- incremented for every release.
--define(RELEASE_NUMBER, 85).
+-define(RELEASE_NUMBER, 86).
 
 -define(DEFAULT_REQUEST_HEADERS,
 	[
