1e5066850d522434e9bdfd47ed5de5fa4d194ac6
Mathieu K <mathieu@arweave.org>
2025-09-18 14:26:51 +0000

    add notes and improve arguments and configuration file management

--- apps/arweave_config/src/arweave_config_file.erl
+++ apps/arweave_config/src/arweave_config_file.erl
@@ -1,22 +1,72 @@
 %%%===================================================================
-%%% @doc
+%%% @doc Arweave configuration file manager.
+%%%
+%%% This module/process is in charge to manage arweave configuration
+%%% file(s). 
+%%%
+%%% == Procedure ==
+%%%
+%%% 1. the process will check if a configuration file can be found
+%%% from the actual parameter if defined. This parameter can be set at
+%%% startup.
+%%%
+%%% 2. if a configuration file is found, then it will try to parse it
+%%% and those the parameters locally
+%%%
+%%% 3. finally, it will set each parameter with their values.
+%%%
+%%% == TODO ==
+%%%
 %%% @end
 %%%===================================================================
 -module(arweave_config_file).
 -behavior(gen_server).
--export([load/0]).
--export([start_link/0]).
+-export([load/0, load/1, reset/0]).
+-export([start_link/0, start_link/1]).
 -export([init/1, terminate/2]).
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 -include_lib("kernel/include/logger.hrl").
 
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+	start_link(#{}).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+start_link(Args) ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+%%--------------------------------------------------------------------
+%% @doc load a configuration file from path configured in the
+%% parameters.
+%% @end
+%%--------------------------------------------------------------------
 load() ->
-	todo.
+	gen_server:cast(?MODULE, load).
 
-init(_) ->
+%%--------------------------------------------------------------------
+%% @doc load a configuration file from a specific path.
+%% @end
+%%--------------------------------------------------------------------
+load(Path) ->
+	gen_server:cast(?MODULE, {load, Path}).
+
+%%--------------------------------------------------------------------
+%% @doc reset the parameters from configuration file.
+%% @end
+%%--------------------------------------------------------------------
+reset() ->
+	gen_server:cast(?MODULE, reset).
+
+%%--------------------------------------------------------------------
+%% @hidden
+%%--------------------------------------------------------------------
+init(#{ parameter := Parameter }) ->
 	% 1. the configuration file should be configured from the
 	%    parameters (via environment or arguments), if not
 	%    simply stop the execution. it means we don't use it.
@@ -28,12 +78,25 @@ init(_) ->
 	%    the configuration file and the parameters, in particular
 	%    for the export/import/save function. For now, stopping
 	%    it if everything is fine should do the job.
-	{stop, normal}.
+	{stop, normal};
+init(_) -> {stop, normal}.
 
+%%--------------------------------------------------------------------
+%% @hidden
+%%--------------------------------------------------------------------
 terminate(_, _) -> ok.
 
+%%--------------------------------------------------------------------
+%% @hidden
+%%--------------------------------------------------------------------
 handle_call(_, _, State) -> {noreply, State}.
 
+%%--------------------------------------------------------------------
+%% @hidden
+%%--------------------------------------------------------------------
 handle_cast(_, State) -> {noreply, State}.
 
+%%--------------------------------------------------------------------
+%% @hidden
+%%--------------------------------------------------------------------
 handle_info(_, State) -> {noreply, State}.
