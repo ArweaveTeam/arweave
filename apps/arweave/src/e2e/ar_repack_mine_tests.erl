623a953d5a5ac875a0d1eb69da74ea3cbb9bd97e
Lev Berman <ldmberman@proton.me>
2025-10-30 19:16:57 +0100

    Allow cross-module replica 2.9 repacking

--- apps/arweave/src/e2e/ar_repack_mine_tests.erl
+++ apps/arweave/src/e2e/ar_repack_mine_tests.erl
@@ -12,9 +12,10 @@
 repack_mine_test_() ->
 	Timeout = ?REPACK_MINE_TEST_TIMEOUT,
 	[
-		{timeout, Timeout, {with, {replica_2_9, replica_2_9}, [fun test_repacking_blocked/1]}},
-		{timeout, Timeout, {with, {replica_2_9, spora_2_6}, [fun test_repacking_blocked/1]}},
-		{timeout, Timeout, {with, {replica_2_9, unpacked}, [fun test_repacking_blocked/1]}},
+		{timeout, Timeout, {with, {replica_2_9, replica_2_9}, [fun test_repack_mine/1]}},
+		{timeout, Timeout, {with, {replica_2_9, spora_2_6}, [fun test_repack_mine/1]}},
+		{timeout, Timeout, {with, {replica_2_9, unpacked}, [fun test_repack_mine/1]}},
+
 		{timeout, Timeout, {with, {unpacked, replica_2_9}, [fun test_repack_mine/1]}},
 		{timeout, Timeout, {with, {unpacked, spora_2_6}, [fun test_repack_mine/1]}},
 		{timeout, Timeout, {with, {spora_2_6, replica_2_9}, [fun test_repack_mine/1]}},
@@ -91,44 +92,6 @@ test_repack_mine({FromPackingType, ToPackingType}) ->
 			%% All of partition 3 is still above the disk pool threshold
 			ar_e2e:assert_empty_partition(RepackerNode, 3, ToPacking)
 	end.
-
-test_repacking_blocked({FromPackingType, ToPackingType}) ->
-	ar_e2e:delayed_print(<<" ~p -> ~p ">>, [FromPackingType, ToPackingType]),
-	?LOG_INFO([{event, test_repacking_blocked}, {module, ?MODULE},
-		{from_packing_type, FromPackingType}, {to_packing_type, ToPackingType}]),
-	ValidatorNode = peer1,
-	RepackerNode = peer2,
-	ar_test_node:stop(ValidatorNode),
-	ar_test_node:stop(RepackerNode),
-	{Blocks, _AddrA, Chunks} = ar_e2e:start_source_node(
-		RepackerNode, FromPackingType, wallet_a),
-
-	[B0 | _] = Blocks,
-	start_validator_node(ValidatorNode, RepackerNode, B0),
-
-	{WalletB, StorageModules} = ar_e2e:source_node_storage_modules(
-		RepackerNode, ToPackingType, wallet_b),
-	AddrB = case WalletB of
-		undefined -> undefined;
-		_ -> ar_wallet:to_address(WalletB)
-	end,
-	ToPacking = ar_e2e:packing_type_to_packing(ToPackingType, AddrB),
-	{ok, Config} = ar_test_node:get_config(RepackerNode),
-	ar_test_node:restart_with_config(RepackerNode, Config#config{
-		storage_modules = Config#config.storage_modules ++ StorageModules,
-		mining_addr = AddrB
-	}),
-
-	ar_e2e:assert_empty_partition(RepackerNode, 1, ToPacking),
-	ar_e2e:assert_no_chunks(RepackerNode, Chunks),
-
-	ar_test_node:restart_with_config(RepackerNode, Config#config{
-		storage_modules = StorageModules,
-		mining_addr = AddrB
-	}),
-
-	ar_e2e:assert_empty_partition(RepackerNode, 1, ToPacking),
-	ar_e2e:assert_no_chunks(RepackerNode, Chunks).
 
 start_validator_node(ValidatorNode, RepackerNode, B0) ->
 	{ok, Config} = ar_test_node:get_config(ValidatorNode),
