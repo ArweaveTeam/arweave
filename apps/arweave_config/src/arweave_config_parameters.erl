bbcfcb419c535ebd045e0ecfb443ea795402ecb5
humaite <mathieu@arweave.org>
2025-11-27 07:24:52 +0000

    Integrate HTTP Logging in Arweave Config
    Lot of new parameters have been added in arweave_config to configure
    logger values. Those are not enabled yet, but have been created to test
    logging integration in arweave_config..
    
    Add a new arweave_config_spec callback to know if a parameter is
    enabled or not.
    
    see: https://github.com/ArweaveTeam/arweave-dev/issues/1006

--- apps/arweave_config/src/arweave_config_parameters.erl
+++ apps/arweave_config/src/arweave_config_parameters.erl
@@ -12,6 +12,7 @@
 %%% @end
 %%%===================================================================
 -module(arweave_config_parameters).
+-compile(warnings_as_errors).
 -export([init/0]).
 -include_("arweave_config.hrl").
 
@@ -22,6 +23,7 @@
 init() ->
 	[
 		#{
+			enabled => true,
 			parameter_key => [data,directory],
 			default => "./data",
 			runtime => false,
@@ -38,6 +40,7 @@ init() ->
 			handle_set => fun legacy_set/3
 		},
 	 	#{
+			enabled => true,
 			parameter_key => [debug],
 			default => false,
 			runtime => true,
@@ -59,19 +62,252 @@ init() ->
 						{false, false} ->
 							ignore;
 						{false, true} ->
-							logger:set_application_level(arweave_config, none),
-							logger:set_application_level(arweave, none),
+							logger:set_application_level(arweave_config, info),
+							logger:set_application_level(arweave, info),
+							ar_logger:start_handler(arweave_debug),
 							legacy_set(K, V, S);
 						{true, false} ->
 							logger:set_application_level(arweave_config, debug),
 							logger:set_application_level(arweave, debug),
+							ar_logger:stop_handler(arweave_debug),
 							legacy_set(K, V, S)
 					end;
 				(K, V, S) ->
 					logger:set_application_level(arweave_config, debug),
 					logger:set_application_level(arweave, debug),
+					ar_logger:start_handler(arweave_debug),
 					legacy_set(K, V, S)
 			end
+		},
+
+		%-----------------------------------------------------
+		% arweave logging feature
+		%-----------------------------------------------------
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_formatter.html
+			% this parameter is not used yet.
+			% TODO: create a special type for this
+			% parameter, a templating like language will
+			% be required to define this parameter from
+			% the configuration or command line, something
+			% like:
+			%   "%time [%level] %msg\n"
+			% will produce:
+			%   [time," [",level,"] ",msg,"\n"]
+			% where %xyz is a defined and valid atom.
+			parameter_key => [logging,formatter,template],
+			default => [time," [",level,"] ",mfa,":",line," ",msg,"\n"],
+			% type => logging_template,
+			environment => false,
+			runtime => false
+		},
+		#{
+			enabled => true,
+			% see: https://www.erlang.org/doc/apps/kernel/logger.html
+			% config.path must be a string (list of
+			% integer). By default, type path will convert
+			% its input in binary. So, one can convert the
+			% value stored using handle_get, or converting
+			% the value when using handle_set.
+			parameter_key => [logging,path],
+			default => "./logs",
+			type => path,
+			environment => true,
+			runtime => false,
+			handle_set => fun
+				(_K, Path, _S) when is_list(Path) ->
+					{store, Path};
+				(_K, Path, _S) when is_binary(Path) ->
+					{store, binary_to_list(Path)}
+			end
+		},
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_formatter.html
+			% TODO: this parameter can also be an atom
+			% (unlimited).
+			parameter_key => [logging,formatter,max_size],
+			default => 8128,
+			type => pos_integer,
+			environmnet => true,
+			runtime => true
+		},
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_formatter.html
+			% TODO: this parameter can also be an atom
+			% (unlimited)
+			parameter_key => [logging,formatter,depth],
+			default => 256,
+			type => pos_integer,
+			environment => true,
+			runtime => true
+		},
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_formatter.html
+			% TODO: this parameter can also be an atom
+			% (unlimited)
+			parameter_key => [logging,formatter,chars_limit],
+			default => 16256,
+			type => pos_integer,
+			environment => true,
+			runtime => true
+		},
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_std_h.html
+			parameter_key => [logging,max_no_files],
+			default => 10,
+			type => pos_integer,
+			environment => true,
+			runtime => true
+		},
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_std_h.html
+			% TODO: this parameter can also be an atom
+			% (infinity)
+			parameter_key => [logging,max_no_bytes],
+			default => 51418800,
+			type => pos_integer,
+			environment => true,
+			runtime => true
+		},
+		#{
+			enabled => {false, wip},
+			% see: https://www.erlang.org/doc/apps/kernel/logger_std_h.html
+			parameter_key => [logging,compress_on_rotate],
+			default => false,
+			type => boolean,
+			environment => true,
+			runtime => true,
+			% this is an example to show how to update all
+			% running handlers during runtime.
+			handle_set => fun
+				(_K, V, _S) ->
+					Handlers = ar_logger:started_handlers(),
+					[
+						begin
+							{ok, C} = logger:get_handler_config(H),
+							C2 = maps:get(config, C),
+							C3 = C2#{ compress_on_rotate => V },
+							logger:set_handler_config(H, config, C3)
+						end
+						||
+						H <- Handlers
+					]
+			end
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,sync_mode_qlen]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,drop_mode_qlen]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,flush_qlen]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,burst_limit_enable]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,burst_limit_max_count]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,burst_limit_window_time]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,overload_kill_enable]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,overload_kill_qlen]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,overload_kill_mem_size]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,overload_kill_restart_after]
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,prefix],
+			default => "arweave",
+			type => string,
+			environment => true,
+			runtime => false
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,handlers,console],
+			default => true,
+			type => boolean,
+			environment => true,
+			runtime => false
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,handlers,info],
+			default => true,
+			type => boolean,
+			environment => true,
+			runtime => true,
+			handle_set => fun
+				(_K,true,_S) ->
+					ar_logger:start_handler(arweave_info),
+					{store, true};
+				(_K,false,_S) ->
+					ar_logger:stop_handler(arweave_info),
+					{store, false}
+			end
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,handlers,debug],
+			default => false,
+			type => boolean,
+			environment => true,
+			runtime => true,
+			handle_set => fun
+				(_K,true,_S) ->
+					ar_logger:start_handler(arweave_debug),
+					{store, true};
+				(_K,false,_S) ->
+					ar_logger:stop_handler(arweave_debug),
+					{store, false}
+			end
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,handlers,http,api],
+			default => false,
+			type => boolean,
+			environment => true,
+			runtime => true,
+			handle_set => fun
+				(_K,true,_S) ->
+					ar_logger:start_handler(arweave_http_api),
+					{store, true};
+				(_K,false,_S) ->
+					ar_logger:stop_handler(arweave_http_api),
+					{store, false}
+			end
+		},
+		#{
+			enabled => {false, wip},
+			parameter_key => [logging,handlers,http,api,compress_on_rotate],
+			baseline => [logging,compress_on_rotate]
 		},
 
 		%-----------------------------------------------------
