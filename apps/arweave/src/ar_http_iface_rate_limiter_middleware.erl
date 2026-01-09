4b8530a5faa20cbb4e416ecff0d0e86a52e0d383
Kristof Hetzl <kristof.hetzl@gmail.com>
2026-01-09 12:03:23 +0000

    route requrests to appropriate rate limiting groups

--- apps/arweave/src/ar_http_iface_rate_limiter_middleware.erl
+++ apps/arweave/src/ar_http_iface_rate_limiter_middleware.erl
@@ -28,23 +28,13 @@ get_limiter_ref(Req) ->
             local_peers;
         _ ->
             Path = ar_http_iface_server:split_path(cowboy_req:path(Req)),
-            case Path of
-                [<<"metrics">> | _ ] ->
-                    metrics;
-                _ ->
-                    general
-            end
+            path_to_limiter_ref(Path)
     end.
 
 reject(Req, _Reason, _Data) ->
     cowboy_req:reply(
       429,
-      #{
-        %% TODO: FILL IN these fields in a reasonable manner
-        %<<"connection">> => <<"close">>,
-        %<<"retry-after">> => integer_to_binary(?THROTTLE_PERIOD div 1000),
-        %<<"x-rate-limit-limit">> => integer_to_binary(Limit)
-       },
+      #{},
       <<"Too Many Requests">>,
       Req
      ).
@@ -54,3 +44,26 @@ get_peer_key(Req) ->
     {A, B, C, D, Port}.
 
 config_peer_to_ip_addr({A, B, C, D, _}) -> {A, B, C, D}.
+
+path_to_limiter_ref([<<"chunk">> | _]) -> chunk;
+path_to_limiter_ref([<<"chunk2">> | _]) -> chunk;
+path_to_limiter_ref([<<"data_sync_record">> | _]) -> data_sync_record;
+path_to_limiter_ref([<<"recent_hash_list_diff">> | _]) -> recent_hash_list_diff;
+path_to_limiter_ref([<<"hash_list">>]) -> block_index;
+path_to_limiter_ref([<<"hash_list2">>]) -> block_index;
+path_to_limiter_ref([<<"block_index">>]) -> block_index;
+path_to_limiter_ref([<<"block_index2">>]) -> block_index;
+path_to_limiter_ref([<<"block">>, _Type, _ID, <<"hash_list">>]) -> block_index;
+path_to_limiter_ref([<<"wallet_list">>]) -> wallet_list;
+path_to_limiter_ref([<<"block">>, _Type, _ID, <<"wallet_list">>]) -> wallet_list;
+path_to_limiter_ref([<<"vdf">>]) -> get_vdf;
+path_to_limiter_ref([<<"vdf">>, <<"session">>]) -> get_vdf_session;
+path_to_limiter_ref([<<"vdf2">>, <<"session">>]) -> get_vdf_session;
+path_to_limiter_ref([<<"vdf3">>, <<"session">>]) -> get_vdf_session;
+path_to_limiter_ref([<<"vdf4">>, <<"session">>]) -> get_vdf_session;
+path_to_limiter_ref([<<"vdf">>, <<"previous_session">>]) -> get_previous_vdf_session;
+path_to_limiter_ref([<<"vdf2">>, <<"previous_session">>]) -> get_previous_vdf_session;
+%% No vdf3 prev_session in ar_blacklist_middleware.hrl ?RPM_BY_PATH
+path_to_limiter_ref([<<"vdf4">>, <<"previous_session">>]) -> get_previous_vdf_session;
+path_to_limiter_ref([<<"metrics">> | _ ])-> metrics;
+path_to_limiter_ref(_) -> general.
