a6dff597858b866785bc6ec574fb5431dded58d7
James Piechota <piechota@gmail.com>
2026-01-10 21:40:24 +0000

    test: fix new large storage module e2e test

--- apps/arweave/src/e2e/ar_sync_pack_mine_tests.erl
+++ apps/arweave/src/e2e/ar_sync_pack_mine_tests.erl
@@ -119,6 +119,12 @@ replica_2_9_small_module_test_() ->
 				]
 		end}.
 
+%% NOTE: the test_large_module_unaligned_sync_pack_mine test should always be 
+%% run after the test_large_module_aligned_sync_pack_mine test. This is because
+%% once a block is mined it shifts the disk pool threshold and changes the
+%% expected syncable chunks. The test_large_module_unaligned_sync_pack_mine test
+%% assumes the higher disk pool threshold in its assertions.
+
 unpacked_large_module_test_() ->
 	{setup, fun () -> setup_source_node(unpacked) end, 
 		fun (GenesisData) ->
@@ -130,7 +136,7 @@ unpacked_large_module_test_() ->
 				]
 	end}.
 	
-spora_2_6_large_module_test_() ->
+replica_2_9_large_module_test_() ->
 	{setup, fun () -> setup_source_node(replica_2_9) end, 
 		fun (GenesisData) ->
 				[
@@ -141,7 +147,7 @@ spora_2_6_large_module_test_() ->
 				]
 		end}.
 
-replica_2_9_large_module_test_() ->
+spora_2_6_large_module_test_() ->
 	{setup, fun () -> setup_source_node(spora_2_6) end, 
 		fun (GenesisData) ->
 				[
@@ -446,9 +452,65 @@ test_small_module_unaligned_sync_pack_mine({{Blocks, Chunks, SourcePackingType},
 	ar_e2e:assert_mine_and_validate(SinkNode, SourceNode, SinkPacking),
 	ok.
 
+
 test_large_module_aligned_sync_pack_mine({{Blocks, Chunks, SourcePackingType}, SinkPackingType}) ->
 	ar_e2e:delayed_print(<<" ~p -> ~p ">>, [SourcePackingType, SinkPackingType]),
 	?LOG_INFO([{event, test_large_module_aligned_sync_pack_mine}, {module, ?MODULE},
+		{from_packing_type, SourcePackingType}, {to_packing_type, SinkPackingType}]),
+	[B0 | _] = Blocks,
+	SourceNode = peer1,
+	SinkNode = peer2,
+
+	Wallet = ar_test_node:remote_call(SinkNode, ar_e2e, load_wallet_fixture, [wallet_b]),
+	SinkAddr = ar_wallet:to_address(Wallet),
+	SinkPacking = ar_e2e:packing_type_to_packing(SinkPackingType, SinkAddr),
+	{ok, Config} = ar_test_node:get_config(SinkNode),
+
+	ModuleSize = floor(2 * ar_block:partition_size()),
+	Module = {ModuleSize, 0, SinkPacking},
+	StoreID = ar_storage_module:id(Module),
+	StorageModules = [ Module ],
+
+	Config2 = Config#config{
+		peers = [ar_test_node:peer_ip(SourceNode)],
+		start_from_latest_state = true,
+		storage_modules = StorageModules,
+		auto_join = true,
+		mining_addr = SinkAddr
+	},
+	?assertEqual(ar_test_node:peer_name(SinkNode),
+		ar_test_node:start_other_node(SinkNode, B0, Config2, true)
+	),
+
+	RangeStart = 0,
+	RangeEnd = ModuleSize,
+
+	%% Make sure the expected data was synced
+	ar_e2e:assert_syncs_range(SinkNode, SinkPacking, RangeStart, RangeEnd),
+	%% The assert_partition_size logic uses ar_mining_stats as the data source.
+	%% It does not currently handle large storage modules well - it attributes all
+	%% chunks in a large storage module to the first partition covered. So we'll
+	%% just assert on partition 0 and ignore partition 1. It is worth keeping
+	%% assert_partition_size in addition to  assert_syncs_range despite this limitation
+	%% as it provides some coverage of the v2_index_data_size_by_packing metric (which
+	%% is relied on by miners).
+	ar_e2e:assert_partition_size(SinkNode, 0, SinkPacking, 4456448),
+	ar_e2e:assert_empty_partition(SinkNode, 1, unpacked_padded),
+	ar_e2e:assert_empty_partition(SinkNode, 1, unpacked),
+	ar_e2e:assert_chunks(SinkNode, SinkPacking, lists:sublist(Chunks, 7, 2)),
+
+	%% Make sure entropy was generated for the module range
+	ar_e2e:assert_has_entropy(SinkNode, RangeStart, RangeEnd, StoreID),
+
+	%% Make sure the data is minable
+	ar_e2e:assert_mine_and_validate(SinkNode, SourceNode, SinkPacking),
+	ok.
+
+%% @doc Test a large storage module that is unaligned with the partition (it starts in the
+%% middle of partition 1 and covers through the end of partition 2).
+test_large_module_unaligned_sync_pack_mine({{Blocks, Chunks, SourcePackingType}, SinkPackingType}) ->
+	ar_e2e:delayed_print(<<" ~p -> ~p ">>, [SourcePackingType, SinkPackingType]),
+	?LOG_INFO([{event, test_large_module_unaligned_sync_pack_mine}, {module, ?MODULE},
 		{from_packing_type, SourcePackingType}, {to_packing_type, SinkPackingType}]),
 	[B0 | _] = Blocks,
 	SourceNode = peer1,
@@ -480,8 +542,19 @@ test_large_module_aligned_sync_pack_mine({{Blocks, Chunks, SourcePackingType}, S
 
 	%% Make sure the expected data was synced
 	ar_e2e:assert_syncs_range(SinkNode, SinkPacking, RangeStart, RangeEnd),
-	ar_e2e:assert_partition_size(SinkNode, 1, SinkPacking),
-	ar_e2e:assert_partition_size(SinkNode, 2, SinkPacking),
+	%% The assert_partition_size logic uses ar_mining_stats as the data source.
+	%% It does not currently handle large storage modules well - it attributes all
+	%% chunks in a large storage module to the first partition covered. So we'll
+	%% just assert on partition 1 and ignore partition 2. It is worth keeping
+	%% assert_partition_size in addition to  assert_syncs_range despite this limitation
+	%% as it provides some coverage of the v2_index_data_size_by_packing metric (which
+	%% is relied on by miners).
+	%% 
+	%% Also note: this test assumes that it runs after the
+	%% test_large_module_aligned_sync_pack_mine test - and therefore the blockchain is
+	%% at height 6 rather than the default 5. This shifts the disk pool threshold and
+	%% allows peer2 to fully sync its large storage module.
+	ar_e2e:assert_partition_size(SinkNode, 1, SinkPacking, 3407872),
 	ar_e2e:assert_empty_partition(SinkNode, 0, SinkPacking),
 	ar_e2e:assert_empty_partition(SinkNode, 1, unpacked_padded),
 	ar_e2e:assert_empty_partition(SinkNode, 1, unpacked),
@@ -489,61 +562,6 @@ test_large_module_aligned_sync_pack_mine({{Blocks, Chunks, SourcePackingType}, S
 
 	%% Make sure entropy was generated for the module range
 	ar_e2e:assert_has_entropy(SinkNode, RangeStart, RangeEnd, StoreID),
-
-	%% Make sure the data is minable
-	ar_e2e:assert_mine_and_validate(SinkNode, SourceNode, SinkPacking),
-	ok.
-
-test_large_module_unaligned_sync_pack_mine({{Blocks, Chunks, SourcePackingType}, SinkPackingType}) ->
-	ar_e2e:delayed_print(<<" ~p -> ~p ">>, [SourcePackingType, SinkPackingType]),
-	?LOG_INFO([{event, test_large_module_unaligned_sync_pack_mine}, {module, ?MODULE},
-		{from_packing_type, SourcePackingType}, {to_packing_type, SinkPackingType}]),
-	[B0 | _] = Blocks,
-	SourceNode = peer1,
-	SinkNode = peer2,
-
-	Wallet = ar_test_node:remote_call(SinkNode, ar_e2e, load_wallet_fixture, [wallet_b]),
-	SinkAddr = ar_wallet:to_address(Wallet),
-	SinkPacking = ar_e2e:packing_type_to_packing(SinkPackingType, SinkAddr),
-	{ok, Config} = ar_test_node:get_config(SinkNode),
-
-	%% Storage module 2x the size of a partition, unaligned (starts at middle of partition 0)
-	%% To achieve an unaligned start with integer partition index, we use 2.5*partition size
-	%% with index 1, giving offset = 2.5*partition (middle of partition 2)
-	%% But data may not extend that far, so instead we use 1.5*partition which still tests
-	%% a module larger than a partition with an unaligned start.
-	Module = {floor(1.5 * ar_block:partition_size()), 1, SinkPacking},
-	StoreID = ar_storage_module:id(Module),
-	StorageModules = [ Module ],
-
-	Config2 = Config#config{
-		peers = [ar_test_node:peer_ip(SourceNode)],
-		start_from_latest_state = true,
-		storage_modules = StorageModules,
-		auto_join = true,
-		mining_addr = SinkAddr
-	},
-	?assertEqual(ar_test_node:peer_name(SinkNode),
-		ar_test_node:start_other_node(SinkNode, B0, Config2, true)
-	),
-
-	%% Module covers [1.5*partition, 3*partition) - starts in middle of partition 1
-	RangeStart = floor(1.5 * ar_block:partition_size()),
-	_RangeEnd = floor(3 * ar_block:partition_size()),
-
-	%% Make sure the expected data was synced
-	%% Data exists in [1.5*partition, 2*partition) range within this module
-	ar_e2e:assert_syncs_range(SinkNode, RangeStart, floor(2 * ar_block:partition_size())),
-	ar_e2e:assert_empty_partition(SinkNode, 1, unpacked_padded),
-	ar_e2e:assert_empty_partition(SinkNode, 1, unpacked),
-	%% Chunks 5-8 are in the second half of partition 1
-	ar_e2e:assert_chunks(SinkNode, SinkPacking, lists:sublist(Chunks, 5, 4)),
-
-	%% Make sure entropy was generated for the module range
-	AlignedStart = ar_util:floor_int(RangeStart, ?DATA_CHUNK_SIZE),
-	AlignedEnd = ar_util:ceil_int(floor(2 * ar_block:partition_size()), ?DATA_CHUNK_SIZE) + ?DATA_CHUNK_SIZE,
-	ar_e2e:assert_has_entropy(SinkNode, AlignedStart, AlignedEnd, StoreID),
-	ar_e2e:assert_no_entropy(SinkNode, 0, AlignedStart, StoreID),
 
 	%% Make sure the data is minable
 	ar_e2e:assert_mine_and_validate(SinkNode, SourceNode, SinkPacking),
