954a4d2559c035fab1f3bbd3c48613095b612a27
Kristof Hetzl <kristof.hetzl@gmail.com>
2026-01-14 12:51:23 +0000

    fix config issue for limiter

--- apps/arweave_limiter/src/arweave_limiter_sup.erl
+++ apps/arweave_limiter/src/arweave_limiter_sup.erl
@@ -60,7 +60,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.chunk.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.chunk.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.chunk.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.chunk.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.chunk.is_manual_reduction_disabled'},
 
      #{id => data_sync_record,
        sliding_window_limit => Config#config.'http_api.limiter.data_sync_record.sliding_window_limit',
@@ -72,7 +72,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.data_sync_record.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.data_sync_record.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.data_sync_record.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.data_sync_record.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.data_sync_record.is_manual_reduction_disabled'},
 
      #{id => recent_hash_list_diff,
        sliding_window_limit => Config#config.'http_api.limiter.recent_hash_list_diff.sliding_window_limit',
@@ -84,7 +84,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.recent_hash_list_diff.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.recent_hash_list_diff.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.recent_hash_list_diff.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.recent_hash_list_diff.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.recent_hash_list_diff.is_manual_reduction_disabled'},
 
      #{id => block_index,
        sliding_window_limit => Config#config.'http_api.limiter.block_index.sliding_window_limit',
@@ -96,7 +96,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.block_index.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.block_index.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.block_index.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.block_index.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.block_index.is_manual_reduction_disabled'},
 
      #{id => wallet_list,
        sliding_window_limit => Config#config.'http_api.limiter.wallet_list.sliding_window_limit',
@@ -108,7 +108,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.wallet_list.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.wallet_list.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.wallet_list.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.wallet_list.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.wallet_list.is_manual_reduction_disabled'},
 
      #{id => get_vdf,
        sliding_window_limit => Config#config.'http_api.limiter.get_vdf.sliding_window_limit',
@@ -120,7 +120,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.get_vdf.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.get_vdf.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.get_vdf.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.get_vdf.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.get_vdf.is_manual_reduction_disabled'},
 
      #{id => get_vdf_session,
        sliding_window_limit => Config#config.'http_api.limiter.get_vdf_session.sliding_window_limit',
@@ -132,7 +132,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.get_vdf_session.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.get_vdf_session.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.get_vdf_session.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.get_vdf_session.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.get_vdf_session.is_manual_reduction_disabled'},
 
      #{id => get_previous_vdf_session,
        sliding_window_limit => Config#config.'http_api.limiter.get_previous_vdf_session.sliding_window_limit',
@@ -145,7 +145,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.get_previous_vdf_session.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.get_previous_vdf_session.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.get_previous_vdf_session.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.get_previous_vdf_session.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.get_previous_vdf_session.is_manual_reduction_disabled'},
 
      #{id => general,
        sliding_window_limit => Config#config.'http_api.limiter.general.sliding_window_limit',
@@ -157,7 +157,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.general.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.general.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.general.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.general.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.general.is_manual_reduction_disabled'},
 
      #{id => metrics,
        sliding_window_limit => Config#config.'http_api.limiter.metrics.sliding_window_limit',
@@ -169,7 +169,7 @@ get_limiter_config() ->
        leaky_rate_limit => Config#config.'http_api.limiter.metrics.leaky_limit',
        leaky_tick_ms => Config#config.'http_api.limiter.metrics.leaky_tick_intercal',
        concurrency_limit => Config#config.'http_api.limiter.metrics.concurrency_limit',
-       is_manual_reduction_disabled => 'http_api.limiter.metrics.is_manual_reduction_disabled'},
+       is_manual_reduction_disabled => Config#config.'http_api.limiter.metrics.is_manual_reduction_disabled'},
      %% Local peers
      #{id => local_peers,
        no_limit => true}
