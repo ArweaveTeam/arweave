7d7da35946dda28574e4cc52e6937343e883879c
Kristof Hetzl <kristof.hetzl@gmail.com>
2026-03-23 13:19:41 +0000

    fix: bump RELEASE_NUMBER

--- apps/arweave/include/ar.hrl
+++ apps/arweave/include/ar.hrl
@@ -33,7 +33,7 @@
 -define(CLIENT_VERSION, 5).
 
 %% The current build number -- incremented for every release.
--define(RELEASE_NUMBER, 89).
+-define(RELEASE_NUMBER, 91).
 
 -define(DEFAULT_REQUEST_HEADERS,
 	[
