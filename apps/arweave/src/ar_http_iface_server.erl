ArweaveTeam/arweave

commit b07b1d8a2aa66138263e094861c1a5bf887b1ac6

Fix Cowboy/Ranch Warning Message (#924)
When starting cowboy/ranch, a warning message was displayed:

    Setting Ranch options together with socket options is deprecated.
    Please use the new map syntax that allows specifying socket options
    separately from other options.

see: https://github.com/ArweaveTeam/arweave-dev/issues/1019

apps/arweave/src/ar_http_iface_server.erl
@@ -91,24 +91,25 @@ start_http_iface_listener(Config) ->
 	Dispatch = cowboy_router:compile([{'_', ?HTTP_IFACE_ROUTES}]),
 	TlsCertfilePath = Config#config.tls_cert_file,
 	TlsKeyfilePath = Config#config.tls_key_file,
-	TransportOpts = [
+	TransportOpts = #{
 		% ranch_tcp parameters
-		{backlog, Config#config.'http_api.tcp.backlog'},
-		{delay_send, Config#config.'http_api.tcp.delay_send'},
-		{keepalive, Config#config.'http_api.tcp.keepalive'},
-		{linger, {
+		backlog => Config#config.'http_api.tcp.backlog',
+		delay_send => Config#config.'http_api.tcp.delay_send',
+		keepalive => Config#config.'http_api.tcp.keepalive',
+		linger => {
 				Config#config.'http_api.tcp.linger',
 				Config#config.'http_api.tcp.linger_timeout'
-			}
 		},
-		{max_connections, Config#config.'http_api.tcp.max_connections'},
-		{nodelay, Config#config.'http_api.tcp.nodelay'},
-		{num_acceptors, Config#config.'http_api.tcp.num_acceptors'},
-		{port, Config#config.port},
-		{send_timeout_close, Config#config.'http_api.tcp.send_timeout_close'},
-		{send_timeout, Config#config.'http_api.tcp.send_timeout'},
-		{shutdown, Config#config.'http_api.tcp.listener_shutdown'}
-	],
+		max_connections => Config#config.'http_api.tcp.max_connections',
+		nodelay => Config#config.'http_api.tcp.nodelay',
+		num_acceptors => Config#config.'http_api.tcp.num_acceptors',
+		send_timeout_close => Config#config.'http_api.tcp.send_timeout_close',
+		send_timeout => Config#config.'http_api.tcp.send_timeout',
+		shutdown => Config#config.'http_api.tcp.listener_shutdown',
+		socket_opts => [
+			{port, Config#config.port}
+		]
+	},
 	ProtocolOpts = #{
 		active_n => Config#config.'http_api.http.active_n',
 		inactivity_timeout => Config#config.'http_api.http.inactivity_timeout',

commit 909fdbc62417af5730e2df848d335a386e524189

Fix partial tls support
b07b1d8 broke TLS support introduced in a5a0c4f .
This was identified and fixed by perplexity computer .

apps/arweave/src/ar_http_iface_server.erl
@@ -128,10 +128,13 @@ start_http_iface_listener(Config) ->
 		not_set ->
 			cowboy:start_clear(ar_http_iface_listener, TransportOpts, ProtocolOpts);
 		_ ->
-			cowboy:start_tls(ar_http_iface_listener, TransportOpts ++ [
-				{certfile, TlsCertfilePath},
-				{keyfile, TlsKeyfilePath}
-			], ProtocolOpts)
+			cowboy:start_tls(ar_http_iface_listener,
+				TransportOpts#{socket_opts => [
+					{port, Config#config.port},
+					{certfile, TlsCertfilePath},
+					{keyfile, TlsKeyfilePath}
+				]},
+				ProtocolOpts)
 	end.
 
 name_route([]) ->