403eb0ec7f1241f8e6c230d3b40cb044cdbf7129
Lev Berman <ldmberman@proton.me>
2026-04-29 19:02:50 +0200

    Reject redundant rebase markers

--- apps/arweave/src/ar_poa.erl
+++ apps/arweave/src/ar_poa.erl
@@ -49,6 +49,16 @@ get_data_path_validation_ruleset(BlockStartOffset) ->
 			ar_block:strict_data_split_threshold()).
 
 validate_data_path(DataRoot, Offset, TXSize, DataPath, Chunk) ->
+	case has_redundant_rebase_marker(DataRoot, Offset, TXSize, DataPath, Chunk) of
+		true ->
+			%% Conservatively reject paths with redundant rebase markers as there's no
+			%% productive case for their use.
+			false;
+		false ->
+			validate_data_path2(DataRoot, Offset, TXSize, DataPath, Chunk)
+	end.
+
+validate_data_path2(DataRoot, Offset, TXSize, DataPath, Chunk) ->
 	Base = ar_merkle:validate_path(DataRoot, Offset, TXSize, DataPath, strict_borders_ruleset),
 	Strict = ar_merkle:validate_path(DataRoot, Offset, TXSize, DataPath,
 			strict_data_split_ruleset),
@@ -73,7 +83,8 @@ validate_data_path(DataRoot, Offset, TXSize, DataPath, Chunk) ->
 				false ->
 					false;
 				true ->
-					case EndOffset - StartOffset == byte_size(Chunk) of
+					case EndOffset - StartOffset == byte_size(Chunk)
+							andalso ar_merkle:has_positive_leaf_size(Offset, TXSize, DataPath) of
 						true ->
 							PassesBase = not (Base == false),
 							PassesStrict = not (Strict == false),
@@ -84,6 +95,14 @@ validate_data_path(DataRoot, Offset, TXSize, DataPath, Chunk) ->
 					end
 			end
 	end.
+
+%%% @doc Return true if we recognize there is at least one redundant rebase marker.
+%%% Strip all rebase markers from the proof and return true if the resulting
+%% proof *still* validates.
+has_redundant_rebase_marker(DataRoot, Offset, TXSize, DataPath, Chunk) ->
+	StrippedDataPath = ar_merkle:strip_rebase_markers(DataPath),
+	StrippedDataPath =/= DataPath andalso
+		validate_data_path2(DataRoot, Offset, TXSize, StrippedDataPath, Chunk) =/= false.
 
 %% @doc Validate a proof of access.
 validate(Args) ->
