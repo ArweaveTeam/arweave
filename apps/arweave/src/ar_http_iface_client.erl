f00d58fa59b5d324a9f24558e94b274ab8ce2d1c
James Piechota <piechota@gmail.com>
2026-09-10 18:42:46 +0000

    refactor: move helpers out of tests block

--- apps/arweave/src/ar_http_iface_client.erl
+++ apps/arweave/src/ar_http_iface_client.erl
@@ -1371,6 +1371,40 @@ get_peers(Peer) ->
     end.
 
 
+%% @doc Parse external list of peer addresses
+parse_peer_list(PeerArray) ->
+    parse_peer_list(PeerArray, ?MAX_PEER_LIST_LEN, []).
+
+parse_peer_list(_PeerArray, 0, Acc) ->
+    lists:reverse(Acc);
+parse_peer_list([], _Left, Acc) ->
+    lists:reverse(Acc);
+parse_peer_list([RawPeer | PeerArray], Left, Acc) when is_binary(RawPeer) ->
+    case parse_peer_address(RawPeer) of
+        {ok, ParsedPeer} ->
+            parse_peer_list(PeerArray, Left - 1, [ParsedPeer | Acc]);
+        {error, _} ->
+            parse_peer_list(PeerArray, Left - 1, Acc)
+    end;
+parse_peer_list([_RawPeer | PeerArray], Left, Acc) ->
+    parse_peer_list(PeerArray, Left - 1, Acc).
+
+%% @doc Parse peer addresses from an external source.
+%% NOTE: It might be tempting to use parser function from arweave_util,
+%% but we need an additional validation in the API.
+parse_peer_address(Peer) when is_binary(Peer), byte_size(Peer) =< ?MAX_PEER_ADDRESS_LEN ->
+    parse_peer_address(binary_to_list(Peer));
+parse_peer_address(Peer)
+    when is_list(Peer), length(Peer) =< ?MAX_PEER_ADDRESS_LEN ->
+    try
+        {ok, arweave_util:parse_peer(Peer)}
+    catch
+        _E:Reason ->
+            {error, Reason}
+    end;
+parse_peer_address(_Peer) ->
+    {error, invalid_peer_address}.
+
 %% @doc Process the response of an /block call.
 handle_block_response(_Peer, _Encoding, {ok, {{<<"400">>, _}, _, _, _, _}}) ->
     not_found;
@@ -1554,37 +1588,3 @@ recent_hash_list_diff_parse_error_test() ->
     Response = {ok, {{<<"200">>, <<"OK">>}, [], Body, undefined, undefined}},
     ?assertMatch({error, _},
         handle_get_recent_hash_list_diff_response(Response, HL, undefined_peer)).
-
-%% @doc Parse external list of peer addresses
-parse_peer_list(PeerArray) ->
-    parse_peer_list(PeerArray, ?MAX_PEER_LIST_LEN, []).
-
-parse_peer_list(_PeerArray, 0, Acc) ->
-    lists:reverse(Acc);
-parse_peer_list([], _Left, Acc) ->
-    lists:reverse(Acc);
-parse_peer_list([RawPeer | PeerArray], Left, Acc) when is_binary(RawPeer) ->
-    case parse_peer_address(RawPeer) of
-        {ok, ParsedPeer} ->
-            parse_peer_list(PeerArray, Left - 1, [ParsedPeer | Acc]);
-        {error, _} ->
-            parse_peer_list(PeerArray, Left - 1, Acc)
-    end;
-parse_peer_list([_RawPeer | PeerArray], Left, Acc) ->
-    parse_peer_list(PeerArray, Left - 1, Acc).
-
-%% @doc Parse peer addresses from an external source.
-%% NOTE: It might be tempting to use parser function from arweave_util,
-%% but we need an additional validation in the API.
-parse_peer_address(Peer) when is_binary(Peer), byte_size(Peer) =< ?MAX_PEER_ADDRESS_LEN ->
-    parse_peer_address(binary_to_list(Peer));
-parse_peer_address(Peer)
-    when is_list(Peer), length(Peer) =< ?MAX_PEER_ADDRESS_LEN ->
-    try 
-        {ok, arweave_util:parse_peer(Peer)}
-    catch
-        _E:Reason ->
-            {error, Reason}
-    end;
-parse_peer_address(_Peer) ->
-    {error, invalid_peer_address}.
